ae32c34d34a631f40a10ac72252f638a7b8d979b
jean.deruelle@gmail.com <jean.deruelle@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-04-15 13:02:04 +0000

    Committing Test framework sip servlet applications Date: 2007-11-14 20:46:42
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@757 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ sip-servlets-test-suite/applications/proxy-sip-servlet/src/main/java/org/mobicents/servlet/sip/testsuite/ProxySipServlet.java
@@ -0,0 +1,91 @@
+package org.mobicents.servlet.sip.testsuite;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.sip.Proxy;
+import javax.servlet.sip.SipErrorEvent;
+import javax.servlet.sip.SipErrorListener;
+import javax.servlet.sip.SipFactory;
+import javax.servlet.sip.SipServlet;
+import javax.servlet.sip.SipServletRequest;
+import javax.servlet.sip.SipServletResponse;
+import javax.servlet.sip.SipURI;
+import javax.servlet.sip.URI;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class ProxySipServlet extends SipServlet implements SipErrorListener,
+		Servlet {
+
+	private static Log logger = LogFactory.getLog(ProxySipServlet.class);
+	
+	
+	/** Creates a new instance of SimpleProxyServlet */
+	public ProxySipServlet() {
+	}
+
+	@Override
+	public void init(ServletConfig servletConfig) throws ServletException {
+		logger.info("the proxy sip servlet has been started");
+		super.init(servletConfig);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	protected void doInvite(SipServletRequest request) throws ServletException,
+			IOException {
+
+		logger.info("Got request:\n"
+				+ request.getMethod());		
+		//This is a proxying sample.
+		SipFactory sipFactory = (SipFactory) getServletContext().getAttribute(SIP_FACTORY);
+		
+		URI uri = sipFactory.createAddress("sip:aa@127.0.0.1:5050").getURI();
+		Proxy proxy = request.getProxy();
+		proxy.setOutboundInterface((SipURI)sipFactory.createAddress("sip:proxy@127.0.0.1:5070").getURI());
+		proxy.proxyTo(uri);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	protected void doBye(SipServletRequest request) throws ServletException,
+			IOException {
+
+		logger.info("Got BYE request:\n" + request);
+		SipServletResponse sipServletResponse = request.createResponse(200);
+		sipServletResponse.send();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	protected void doResponse(SipServletResponse response)
+			throws ServletException, IOException {
+
+		logger.info("Got response:\n" + response);
+		super.doResponse(response);
+	}
+
+	// SipErrorListener methods
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void noAckReceived(SipErrorEvent ee) {
+		logger.error("noAckReceived.");
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void noPrackReceived(SipErrorEvent ee) {
+		logger.error("noPrackReceived.");
+	}
+
+}
---
+++ sip-servlets-test-suite/applications/simple-sip-servlet/src/main/java/org/mobicents/servlet/sip/testsuite/SimpleSipServlet.java
@@ -0,0 +1,84 @@
+package org.mobicents.servlet.sip.testsuite;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.sip.SipErrorEvent;
+import javax.servlet.sip.SipErrorListener;
+import javax.servlet.sip.SipServlet;
+import javax.servlet.sip.SipServletRequest;
+import javax.servlet.sip.SipServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class SimpleSipServlet extends SipServlet implements SipErrorListener,
+		Servlet {
+
+	
+	
+	private static Log logger = LogFactory.getLog(SimpleSipServlet.class);
+	
+	
+	/** Creates a new instance of SimpleProxyServlet */
+	public SimpleSipServlet() {
+	}
+
+	@Override
+	public void init(ServletConfig servletConfig) throws ServletException {
+		logger.info("the simple sip servlet has been started");
+		super.init(servletConfig);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	protected void doInvite(SipServletRequest request) throws ServletException,
+			IOException {
+
+		logger.info("Got request:\n"
+				+ request.getMethod());
+		SipServletResponse sipServletResponse = request.createResponse(200);
+		sipServletResponse.send();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	protected void doBye(SipServletRequest request) throws ServletException,
+			IOException {
+
+		logger.info("Got BYE request:\n" + request);
+		SipServletResponse sipServletResponse = request.createResponse(200);
+		sipServletResponse.send();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	protected void doResponse(SipServletResponse response)
+			throws ServletException, IOException {
+
+		logger.info("Got response:\n" + response);
+		super.doResponse(response);
+	}
+
+	// SipErrorListener methods
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void noAckReceived(SipErrorEvent ee) {
+		logger.error("noAckReceived.");
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void noPrackReceived(SipErrorEvent ee) {
+		logger.error("noPrackReceived.");
+	}
+
+}
