e12aec6061c3e782b4bb8e4f1c0f6d3c585c711a
jean.deruelle@gmail.com <jean.deruelle@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-04-15 15:33:24 +0000

    New proxying test Date: 2007-12-19 15:15:51
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@886 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-test-suite/testsuite/src/test/java/org/mobicents/servlet/sip/testsuite/proxy/ProxyTest.java
+++ sip-servlets-test-suite/testsuite/src/test/java/org/mobicents/servlet/sip/testsuite/proxy/ProxyTest.java
@@ -1,9 +1,21 @@
 package org.mobicents.servlet.sip.testsuite.proxy;
 
 
+import java.util.EventObject;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Properties;
+import java.util.TooManyListenersException;
 
+import javax.sip.DialogTerminatedEvent;
+import javax.sip.IOExceptionEvent;
 import javax.sip.ListeningPoint;
+import javax.sip.RequestEvent;
+import javax.sip.ResponseEvent;
+import javax.sip.SipListener;
+import javax.sip.SipProvider;
+import javax.sip.TimeoutEvent;
+import javax.sip.TransactionTerminatedEvent;
 import javax.sip.message.Response;
 
 import org.apache.commons.logging.Log;
@@ -13,22 +25,20 @@ import org.cafesip.sipunit.SipPhone;
 import org.cafesip.sipunit.SipStack;
 import org.mobicents.servlet.sip.SipServletTestCase;
 
-public class ProxyTest extends SipServletTestCase{
+public class ProxyTest extends SipServletTestCase implements SipListener{
 
 	private static Log logger = LogFactory.getLog(ProxyTest.class);
 	
-	private SipStack sipStackSender;
-	private SipStack[] sipStackReceivers;
+	protected Shootist shootist;
+
+	protected Shootme shootme;
 	
-	private SipPhone sipPhoneSender;
-	private SipPhone[] sipPhoneReceivers;
+	protected Hashtable providerTable = new Hashtable();
 	
 	private static final int timeout = 5000;
 	
 	private static final int receiversCount = 1;
 	
-	// Don't restart the server for this set of tests.
-	private static boolean firstTime = true;
 
 	public ProxyTest(String name) {
 		super(name);
@@ -36,28 +46,38 @@ public class ProxyTest extends SipServletTestCase{
 	
 	@Override
 	public void setUp()
-	{
-		if(firstTime)
+	{		try {
+		super.setUp();
+		this.shootist = new Shootist();
+		this.shootme = new Shootme();
+
+	} catch (Exception e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+		
+	}
+	
+	public void testProxy() {
+		this.shootme.init();
+		this.shootist.init();
+		for(int q=0; q<50; q++)
 		{
-			try {
-				super.setUp();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			if(shootist.ended == false)
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 		}
-		firstTime = true;
 	}
 	
 	@Override
 	public void tearDown()
 	{
 		try {
-			sipPhoneSender.dispose();
-			for(SipPhone sp : sipPhoneReceivers) sp.dispose();
-			sipStackSender.dispose();
-			for(SipStack ss : sipStackReceivers) ss.dispose();
-			super.tearDown();
+			//super.tearDown();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -77,124 +97,49 @@ public class ProxyTest extends SipServletTestCase{
 				"org/mobicents/servlet/sip/testsuite/proxy/simple-sip-servlet-dar.properties";
 	}
 	
-	public SipStack makeStack(String transport, int port)
-	{
-		Properties properties = new Properties();
-		String peerHostPort1 = "127.0.0.1:5070";
-		properties.setProperty("javax.sip.OUTBOUND_PROXY", peerHostPort1 + "/"
-				+ "udp");
-		properties.setProperty("javax.sip.STACK_NAME", "UAC_"+transport+"_"+port);
-		properties.setProperty("sipunit.BINDADDR", "127.0.0.1");
-		properties.setProperty("gov.nist.javax.sip.DEBUG_LOG",
-		"logs/simplesipservlettest_debug_port"+port+".txt");
-		properties.setProperty("gov.nist.javax.sip.SERVER_LOG",
-		"logs/simplesipservlettest_log_port"+port+".txt");
-		try {
-			return new SipStack(transport, port, properties);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
-	public void setupPhones() {
-		sipStackReceivers = new SipStack[receiversCount];
-		sipPhoneReceivers = new SipPhone[receiversCount];
-		try {
-			sipStackSender = makeStack(SipStack.PROTOCOL_UDP , 5058);
-			ListeningPoint lp = sipStackSender.getSipProvider().getListeningPoint("udp");
-			String stackIPAddress = lp.getIPAddress();
-			sipPhoneSender = sipStackSender.createSipPhone("localhost",
-					SipStack.PROTOCOL_UDP, 5070, "sip:sender@nist.gov");
-
-			for(int q=0; q<receiversCount; q++)
-			{
-				int port = 5058 - 1 - q;
-				sipStackReceivers[q] = makeStack(SipStack.PROTOCOL_UDP, port);
-				sipPhoneReceivers[q] = sipStackReceivers[q].createSipPhone(
-						"localhost", SipStack.PROTOCOL_UDP, 5070, "sip:receiver@nist.gov");
-			}
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-		
 	public void init()
 	{
-		setupPhones();
+		//setupPhones();
 	}
 
-	// Tests a complete call.
-	public void testCallProxying() throws InterruptedException {
-		init();
-		try{
-			SipCall sender = sipPhoneSender.createSipCall();
-			SipCall[] receiverCalls = new SipCall[receiversCount];
-			for(int q=0; q<receiversCount; q++)
-				receiverCalls[q] = sipPhoneReceivers[q].createSipCall();
-			
-			for(SipCall call:receiverCalls)
-				call.listenForIncomingCall();
-			
-			sender.initiateOutgoingCall("sip:receiver@nist.gov", null);
-			
-			for(SipCall call:receiverCalls)
-				assertTrue(call.waitForIncomingCall(timeout));
-			
-			for(SipCall call:receiverCalls)
-				assertTrue(call.sendIncomingCallResponse(Response.RINGING, "Ringing", 0));
-			assertTrue(sender.waitOutgoingCallResponse(timeout));
-			
-			for(SipCall call:receiverCalls)
-				assertTrue(call.sendIncomingCallResponse(Response.OK, "Answer", 0));
-			assertTrue(sender.waitOutgoingCallResponse(timeout));
-			
-			assertTrue(sender.sendInviteOkAck());
-			
-			for(SipCall call:receiverCalls)
-				assertTrue(call.waitForAck(timeout));
-			
-			assertTrue(sender.disconnect());
-			
-			for(SipCall call:receiverCalls)
-				assertTrue(call.respondToDisconnect());
-			
-		} catch (Exception e) {
-			fail(e.toString());
-			e.printStackTrace();
-		}
+	private SipListener getSipListener(EventObject sipEvent) {
+		SipProvider source = (SipProvider) sipEvent.getSource();
+		SipListener listener = (SipListener) providerTable.get(source);
+		if (listener == null)
+			throw new RuntimeException("Unexpected null listener");
+		return listener;
 	}
-	
-	// This just checks if the INVITEs are getting through
-	public void testInviteProxying() throws InterruptedException {
-		init();
-		try{
-			SipCall sender = sipPhoneSender.createSipCall();
-			SipCall[] receiverCalls = new SipCall[receiversCount];
-			for(int q=0; q<receiversCount; q++)
-				receiverCalls[q] = sipPhoneReceivers[q].createSipCall();
-			
-			for(SipCall call:receiverCalls)
-				call.listenForIncomingCall();
-			
-			sender.initiateOutgoingCall("sip:receiver@nist.gov", null);
-			
-			for(SipCall call:receiverCalls)
-				assertTrue(call.waitForIncomingCall(timeout));
-			
-			for(SipCall call:receiverCalls)
-				assertTrue(call.sendIncomingCallResponse(Response.RINGING, "Ringing", 0));
-			assertTrue(sender.waitOutgoingCallResponse(timeout));
-			
-			for(SipCall call:receiverCalls)
-				assertTrue(call.sendIncomingCallResponse(Response.OK, "Answer", 0));
-			assertTrue(sender.waitOutgoingCallResponse(timeout));
 
-		} catch (Exception e) {
-			fail(e.toString());
-			e.printStackTrace();
-		}
+	public void processRequest(RequestEvent requestEvent) {
+		getSipListener(requestEvent).processRequest(requestEvent);
+
+	}
+
+	public void processResponse(ResponseEvent responseEvent) {
+		getSipListener(responseEvent).processResponse(responseEvent);
+
+	}
+
+	public void processTimeout(TimeoutEvent timeoutEvent) {
+		getSipListener(timeoutEvent).processTimeout(timeoutEvent);
+	}
+
+	public void processIOException(IOExceptionEvent exceptionEvent) {
+		fail("unexpected exception");
+
+	}
+
+	public void processTransactionTerminated(
+			TransactionTerminatedEvent transactionTerminatedEvent) {
+		getSipListener(transactionTerminatedEvent)
+				.processTransactionTerminated(transactionTerminatedEvent);
+
+	}
+
+	public void processDialogTerminated(
+			DialogTerminatedEvent dialogTerminatedEvent) {
+		getSipListener(dialogTerminatedEvent).processDialogTerminated(
+				dialogTerminatedEvent);
+
 	}
 }
