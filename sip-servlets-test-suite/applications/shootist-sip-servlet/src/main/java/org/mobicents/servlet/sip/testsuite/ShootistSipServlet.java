64ded4ef765444a18b36be6d704f9873dc961f30
jean.deruelle@gmail.com <jean.deruelle@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-04-15 13:12:36 +0000

    Adding shootist sip servlet application to test basic application routing when sending out requests and receiving responses Date: 2007-11-18 21:51:41
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@770 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ sip-servlets-test-suite/applications/shootist-sip-servlet/src/main/java/org/mobicents/servlet/sip/testsuite/ShootistSipServlet.java
@@ -0,0 +1,92 @@
+package org.mobicents.servlet.sip.testsuite;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.sip.SipErrorEvent;
+import javax.servlet.sip.SipErrorListener;
+import javax.servlet.sip.SipFactory;
+import javax.servlet.sip.SipServlet;
+import javax.servlet.sip.SipServletContextEvent;
+import javax.servlet.sip.SipServletListener;
+import javax.servlet.sip.SipServletRequest;
+import javax.servlet.sip.SipServletResponse;
+import javax.servlet.sip.SipURI;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+public class ShootistSipServlet extends SipServlet implements SipErrorListener, SipServletListener,
+		Servlet {
+
+	private static Log logger = LogFactory.getLog(ShootistSipServlet.class);
+	
+	
+	/** Creates a new instance of ShootistSipServlet */
+	public ShootistSipServlet() {
+	}
+
+	@Override
+	public void init(ServletConfig servletConfig) throws ServletException {
+		logger.info("the shootist has been started");
+		super.init(servletConfig);
+	}
+	
+	@Override
+	protected void doSuccessResponse(SipServletResponse sipServletResponse)
+			throws ServletException, IOException {
+		logger.info("Got : " + sipServletResponse.getStatus() + " "
+				+ sipServletResponse.getMethod());
+		sipServletResponse.createAck();
+		sipServletResponse.send();		
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	protected void doResponse(SipServletResponse response)
+			throws ServletException, IOException {
+		
+	}
+
+	// SipErrorListener methods
+	
+	/*
+	 * (non-Javadoc)
+	 * @see javax.servlet.sip.SipErrorListener#noAckReceived(javax.servlet.sip.SipErrorEvent)
+	 */
+	public void noAckReceived(SipErrorEvent ee) {
+		logger.error("noAckReceived.");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see javax.servlet.sip.SipErrorListener#noPrackReceived(javax.servlet.sip.SipErrorEvent)
+	 */
+	public void noPrackReceived(SipErrorEvent ee) {
+		logger.error("noPrackReceived.");
+	}
+
+	// SipServletListener methods
+	/*
+	 * (non-Javadoc)
+	 * @see javax.servlet.sip.SipServletListener#servletInitialized(javax.servlet.sip.SipServletContextEvent)
+	 */
+	public void servletInitialized(SipServletContextEvent ce) {
+		SipFactory sipFactory = (SipFactory)ce.getServletContext().getAttribute(SIP_FACTORY);		
+		SipURI fromURI = sipFactory.createSipURI("BigGuy", "here.com");			
+		SipURI toURI = sipFactory.createSipURI("LittleGuy", "there.com");
+		SipServletRequest sipServletRequest = 
+			sipFactory.createRequest(sipFactory.createApplicationSession(), "INVITE", fromURI, toURI);
+//		try {			
+//			sipServletRequest.send();
+//		} catch (IOException e) {
+//			logger.error(e);
+//		}
+		
+	}
+
+}
