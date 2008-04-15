2ef0949aceec30a3ebe3d076bbf260685586de9d
jean.deruelle@gmail.com <jean.deruelle@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-04-15 14:06:39 +0000

    Committing 2 new test applications : Call Blocking (working but basic) and Call Forwarding(not working) Date: 2007-11-28 12:02:55
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@805 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ sip-servlets-test-suite/applications/call-blocking-servlet/src/main/java/org/mobicents/servlet/sip/testsuite/CallBlockingSipServlet.java
@@ -0,0 +1,58 @@
+package org.mobicents.servlet.sip.testsuite;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.sip.SipErrorEvent;
+import javax.servlet.sip.SipErrorListener;
+import javax.servlet.sip.SipServlet;
+import javax.servlet.sip.SipServletRequest;
+import javax.servlet.sip.SipServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+public class CallBlockingSipServlet extends SipServlet implements SipErrorListener,
+		Servlet {
+
+	private static Log logger = LogFactory.getLog(CallBlockingSipServlet.class);
+	
+	
+	/** Creates a new instance of CallBlockingSipServlet */
+	public CallBlockingSipServlet() {}
+
+	@Override
+	public void init(ServletConfig servletConfig) throws ServletException {
+		logger.info("the call blocking sip servlet has been started");
+		super.init(servletConfig);
+	}
+
+	@Override
+	protected void doInvite(SipServletRequest request) throws ServletException,
+			IOException {
+
+		logger.info("Got request:\n"
+				+ request.getMethod());
+		SipServletResponse sipServletResponse = request.createResponse(SipServletResponse.SC_FORBIDDEN);
+		sipServletResponse.send();		
+	}
+
+	// SipErrorListener methods
+	/**
+	 * {@inheritDoc}
+	 */
+	public void noAckReceived(SipErrorEvent ee) {
+		logger.error("noAckReceived.");
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void noPrackReceived(SipErrorEvent ee) {
+		logger.error("noPrackReceived.");
+	}
+
+}
