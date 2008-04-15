e3a1ab04a676bde8a9e90a6aaa929fcd29689c63
jean.deruelle@gmail.com <jean.deruelle@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-04-15 13:02:40 +0000

    Committing Test framework junit tests for sip servlet applications Date: 2007-11-14 20:47:58
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@758 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ sip-servlets-test-suite/testsuite/src/test/java/org/mobicents/servlet/sip/SipServletTestCase.java
@@ -0,0 +1,55 @@
+package org.mobicents.servlet.sip;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+import junit.framework.TestCase;
+
+/**
+ * This class is responsible for reading up the properties configuration file
+ * and starting/stopping tomcat. It delegates to the test case inheriting from it 
+ * the deployment of the context and the location of the dar configuration file
+ * since it should map to the test case.
+ */
+public abstract class SipServletTestCase extends TestCase {
+	protected String tomcatBasePath;
+	protected String projectHome;
+	protected SipEmbedded tomcat;	
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		//Reading properties
+		Properties properties = new Properties();
+		InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(
+				"org/mobicents/servlet/sip/testsuite/testsuite.properties");
+		properties.load(inputStream);		
+		tomcatBasePath = properties.getProperty("tomcat.home");		
+		projectHome = properties.getProperty("project.home");
+		//starting tomcat
+		tomcat = new SipEmbedded();
+		tomcat.setPath(tomcatBasePath);		
+		tomcat.setLoggingFilePath(projectHome + "/sip-servlets-test-suite/testsuite/src/test/resources/logging.properties");
+		String darConfigurationFile = getDarConfigurationFile();
+		tomcat.setDarConfigurationFilePath(darConfigurationFile);
+		tomcat.startTomcat();
+		deployApplication();
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		tomcat.stopTomcat();
+		super.tearDown();
+	}
+
+	/**
+	 * Delegates the choice of the application to deploy to the test case 
+	 */
+	protected abstract void deployApplication();
+	
+	/**
+	 * Delegates the choice of the default application router 
+	 * configuration file to use to the test case
+	 */
+	protected abstract String getDarConfigurationFile();
+}
