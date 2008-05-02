422f77992b4f31f890bbaec0d1f964c4a336ec4a
jean.deruelle <jean.deruelle@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-05-02 21:35:55 +0000

    Modifying perf setup
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@1553 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-test-suite/sipp-scenarios/performance/performance-test.sh
+++ sip-servlets-test-suite/sipp-scenarios/performance/performance-test.sh
@@ -1,1 +1,1 @@
-./sipp 127.0.0.1:5080 -s yousendbye -sf simple-flow/performance-uac.xml -trace_err -i 127.0.0.1 -p 5055 -r 60 -m 100000
+./sipp 127.0.0.1:5080 -s yousendbye -sf simple-flow/performance-uac.xml -trace_err -i 127.0.0.1 -p 5055 -r 40 -m 10000
---
+++ sip-servlets-test-suite/sipp-scenarios/performance/start-tomcat-server-for-perf.sh
@@ -0,0 +1,2 @@
+export CATALINA_OPTS="-Xms1536m -Xmx1536m -XX:PermSize=128M -XX:MaxPermSize=256M -XX:+UseConcMarkSweepGC"
+sh $CATALINA_HOME/bin/catalina.sh run
