b525f1f88296b03bbf84da7683778804d5cd1d7d
jean.deruelle <jean.deruelle@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-10-21 15:10:09 +0000

    Fix for failing hudson job simple call perf test
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@3323 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-test-suite/sipp-scenarios/performance/prepare-tomcat-server-for-perf.sh
+++ sip-servlets-test-suite/sipp-scenarios/performance/prepare-tomcat-server-for-perf.sh
@@ -1,5 +1,5 @@
 cp tomcat-setup/server.xml $CATALINA_HOME/conf/server.xml
 cp tomcat-setup/log4j.xml $CATALINA_HOME/lib/log4j.xml
 mvn clean install -f ../../../sip-servlets-examples/simple-sip-servlet/pom.xml
-cp ../../../sip-servlets-examples/simple-sip-servlet/target/simple-sip-servlet-1.0.war $CATALINA_HOME/webapps
+cp ../../../sip-servlets-examples/simple-sip-servlet/target/simple-sip-servlet-1.1-SNAPSHOT.war $CATALINA_HOME/webapps
 cp ../../../sip-servlets-examples/simple-sip-servlet/simple-dar.properties $CATALINA_HOME/conf/dars/simple-dar.properties
