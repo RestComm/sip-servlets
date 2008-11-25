735fe168ea2f0ab6d888b80d218cc6ff3d879647
jean.deruelle <jean.deruelle@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-11-25 16:39:50 +0000

    Fixing the scripts
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@3873 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-test-suite/sipp-scenarios/performance/b2bua/performance-b2bua-test.sh
+++ sip-servlets-test-suite/sipp-scenarios/performance/b2bua/performance-b2bua-test.sh
@@ -1,4 +1,4 @@
 #Don't forget to kill the sipp uas process after the test run it might still hang
-../sipp 127.0.0.1:5080 -sf call-forwarding-receiver.xml -trace_err -i 127.0.0.1 -p 5090 -bg -timeout 20
-../sipp 127.0.0.1:5080 -s receiver -sf call-forwarding-sender.xml -trace_err -i 127.0.0.1 -p 5050 -r 1 -m 2
+../sipp 127.0.0.1:5080 -sf call-forwarding-receiver.xml -trace_err -i 127.0.0.1 -p 5090 -bg -timeout 30
+../sipp 127.0.0.1:5080 -s receiver -sf call-forwarding-sender.xml -trace_err -i 127.0.0.1 -p 5050 -r 1 -m 1
 sleep 10
