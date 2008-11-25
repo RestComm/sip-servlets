322db22ebd79661a114bf42b73f462551ad47ecc
jean.deruelle <jean.deruelle@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-11-25 13:51:22 +0000

    Upadting sipp scripts to use the latest example version so that there is no need to update it anymore on every release
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@3868 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-test-suite/sipp-scenarios/performance/failover/prepare-jboss-server-for-perf.sh
+++ sip-servlets-test-suite/sipp-scenarios/performance/failover/prepare-jboss-server-for-perf.sh
@@ -4,5 +4,5 @@ cp ../jboss-setup/jboss-service.xml $JBOSS_HOME/server/default/deploy/jboss-web.
 cp ../jboss-setup/webserver-xmbean.xml $JBOSS_HOME/server/default/deploy/jboss-web.deployer/META-INF/webserver-xmbean.xml
 cp ../jboss-setup/log4j.xml $JBOSS_HOME/server/default/conf/jboss-log4j.xml
 mvn clean install -f ../../../../sip-servlets-examples/simple-sip-servlet/pom.xml
-cp ../../../../sip-servlets-examples/simple-sip-servlet/target/simple-sip-servlet-1.0.war $JBOSS_HOME/server/default/deploy
+cp ../../../../sip-servlets-examples/simple-sip-servlet/target/simple-sip-servlet-*.war $JBOSS_HOME/server/default/deploy
 cp ../../../../sip-servlets-examples/simple-sip-servlet/simple-dar.properties $JBOSS_HOME/server/default/conf/dars/simple-dar.properties
--- sip-servlets-test-suite/sipp-scenarios/performance/prepare-tomcat-server-for-perf.sh
+++ sip-servlets-test-suite/sipp-scenarios/performance/prepare-tomcat-server-for-perf.sh
@@ -2,5 +2,5 @@ cp tomcat-setup/mss-sip-stack.properties $CATALINA_HOME/conf/mss-sip-stack.prope
 cp tomcat-setup/server.xml $CATALINA_HOME/conf/server.xml
 cp tomcat-setup/log4j.xml $CATALINA_HOME/lib/log4j.xml
 mvn clean install -f ../../../sip-servlets-examples/simple-sip-servlet/pom.xml
-cp ../../../sip-servlets-examples/simple-sip-servlet/target/simple-sip-servlet-1.1-SNAPSHOT.war $CATALINA_HOME/webapps
+cp ../../../sip-servlets-examples/simple-sip-servlet/target/simple-sip-servlet-*.war $CATALINA_HOME/webapps
 cp ../../../sip-servlets-examples/simple-sip-servlet/simple-dar.properties $CATALINA_HOME/conf/dars/simple-dar.properties
