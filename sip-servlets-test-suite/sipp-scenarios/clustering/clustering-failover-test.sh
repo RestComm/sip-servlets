9510437e64987bc869c3dd1b06f11eb46d0ea0a1
jean.deruelle <jean.deruelle@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2008-12-19 11:09:51 +0000

    updating scripts and config in clustering
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@4064 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-test-suite/sipp-scenarios/clustering/clustering-failover-test.sh
+++ sip-servlets-test-suite/sipp-scenarios/clustering/clustering-failover-test.sh
@@ -1,10 +1,9 @@
-rm ./logs/*
 if [ $# -ne 0 ]; then
 	case $1 in	
 	    proxy)
 	    		rm ./proxy/*.log
 	    		echo "Distributed example used is proxy";
-	    		./sipp 127.0.0.1:5080 -sf proxy/location-service-receiver.xml -i 127.0.0.1 -p 5090 -bg -trace_msg -timeout 20  
+	    		./sipp 127.0.0.1:5080 -sf proxy/location-service-receiver.xml -i 127.0.0.1 -p 5090 -bg -trace_msg -timeout 30  
 	    		./sipp 127.0.0.1:5080 -s receiver-failover -sf proxy/location-service-sender.xml -trace_err -i 127.0.0.1 -p 5050 -r 1 -m 1 -rsa 127.0.0.1:5060 -trace_msg
 	            ;;
 	    b2bua)
@@ -17,7 +16,7 @@ if [ $# -ne 0 ]; then
 	    		rm ./b2bua/*.log
 	            echo "Distributed example used is b2bua-remote-send-bye";
 	            ./sipp 127.0.0.1:5080 -sf b2bua/call-forwarding-receiver-sends-bye.xml -i 127.0.0.1 -p 5090 -bg -trace_msg -timeout 20  
-	    		./sipp 127.0.0.1:5080 -s receiver -sf b2bua/call-forwarding-sender-receives-bye.xml -trace_err -i 127.0.0.1 -p 5050 -r 1 -m 1 -rsa 127.0.0.1:5060 -trace_msg
+	    		./sipp 127.0.0.1:5080 -s receiver -sf b2bua/call-forwarding-sender-receives-bye.xml -trace_err -i 127.0.0.1 -p 5050 -r 1 -m 1 -rsa 127.0.0.1:5060 -trace_msg -nd
 	            ;;
 		uac)
 				rm ./uac/*.log
@@ -27,7 +26,7 @@ if [ $# -ne 0 ]; then
 	    *)
 	    		rm ./uas/*.log
 	            echo "Distributed example used is uas";
-	    		./sipp 127.0.0.1:5080 -s isendbye -sf uas/clustering-uac.xml -trace_err -i 127.0.0.1 -p 5050 -r 15 -m 100000 -rsa 127.0.0.1:5060 -trace_msg -nd
+	    		./sipp 127.0.0.1:5080 -s isendbye -sf uas/clustering-uac.xml -trace_err -i 127.0.0.1 -p 5050 -r 1 -m 1 -rsa 127.0.0.1:5060 -trace_msg -nd
 	            ;;
     esac
 fi
