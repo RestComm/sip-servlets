7fbd565f14159b3d27ae4f3b984acb1241a22a77
jean.deruelle <jean.deruelle@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-01-16 15:05:31 +0000

    Initial import of sip-servlets-annotations project
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@4386 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ sip-servlets-annotations/src/main/java/org/mobicents/servlet/sip/annotation/ConcurrencyControl.java
@@ -0,0 +1,35 @@
+/*
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.mobicents.servlet.sip.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * The @ConcurrencyControl annotation is used to create an application level annotation for a Sip Servlet Application
+ * 
+ * @author jean.deruelle@gmail.com 
+ */
+@Target(ElementType.PACKAGE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ConcurrencyControl {
+
+	ConcurrencyControlMode mode() default ConcurrencyControlMode.SipSession;
+	
+}
