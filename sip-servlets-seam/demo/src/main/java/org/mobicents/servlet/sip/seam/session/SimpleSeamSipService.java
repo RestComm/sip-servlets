2f1def4b8e7adc5a0f8bcba96dc3ac7863267b38
vladimir.ralev <vladimir.ralev@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-01-17 02:15:58 +0000

    Seam Sip/Media Conference Demo
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@4399 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-seam/demo/src/main/java/org/mobicents/servlet/sip/seam/session/SimpleSeamSipService.java
+++ sip-servlets-seam/demo/src/main/java/org/mobicents/servlet/sip/seam/session/SimpleSeamSipService.java
@@ -2,12 +2,15 @@ package org.mobicents.servlet.sip.seam.session;
 
 import javax.servlet.sip.SipServletRequest;
 import javax.servlet.sip.SipServletResponse;
+import javax.servlet.sip.SipSession;
 
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.*;
 import org.jboss.seam.annotations.async.*;
 import org.jboss.seam.annotations.bpm.*;
 import org.jboss.seam.log.Log;
+import org.mobicents.mscontrol.MsConnectionEvent;
+import org.mobicents.servlet.sip.seam.entrypoint.media.MediaController;
 
 
 @Name("simpleSeamSipService")
@@ -15,29 +18,45 @@ import org.jboss.seam.log.Log;
 @Transactional
 public class SimpleSeamSipService {
 	@Logger Log log;
+	@In MediaController mediaController;
+	@In SipSession sipSession;
+	
+	@In(scope=ScopeType.APPLICATION, required=false)
+	@Out(scope=ScopeType.APPLICATION, required=false)
+	String conferenceEndpointName;
+	
 	@Observer("INVITE")
-	@CreateProcess(definition="demo")
-	public void doInvite(SipServletRequest request)
-	throws Exception {
+	@CreateProcess(definition = "demo")
+	public void doInvite(SipServletRequest request) throws Exception {
+		String sdp = new String((byte[]) request.getContent());
 		request.createResponse(180).send();
-		Thread.sleep(100);
+		sipSession.setAttribute("inviteRequest", request);
+		if (conferenceEndpointName == null)
+			conferenceEndpointName = "media/trunk/Conference/$";
+		mediaController.createConnection(conferenceEndpointName).modify("$",
+				sdp);
+	}
+
+	@Observer("connectionOpen")
+	public void doConnectionOpen(MsConnectionEvent event) {
+		conferenceEndpointName = event.getConnection().getEndpoint()
+				.getLocalName();
+		SipServletRequest request = (SipServletRequest) sipSession
+				.getAttribute("inviteRequest");
+		SipServletResponse response = request.createResponse(200);
+		try {
+			response.setContent(event.getConnection().getLocalDescriptor(),
+					"application/sdp");
+			response.send();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Observer( { "BYE", "REGISTER" })
+	public void sayOK(SipServletRequest request) throws Exception {
+		log.info("BYE-received");
 		request.createResponse(200).send();
 	}
 
-	@Observer("ACK")
-	@StartTask(taskId="2") @EndTask()
-	public void doAck(SipServletRequest request)
-	throws Exception {
-	}
-
-	@Observer({"REGISTER","BYE"})
-	public void sayOK(SipServletRequest request)
-	throws Exception {
-		request.createResponse(200).send();
-	}
-
-	@Observer({"RESPONSE"})
-	public void sayOK(SipServletResponse response)
-	throws Exception {
-	}
 }
