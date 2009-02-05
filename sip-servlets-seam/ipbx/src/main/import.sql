a334a6faa1a7392a5f437a22e03d11d7694b7235
vladimir.ralev <vladimir.ralev@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-02-05 03:21:02 +0000

    Add tips to the UI and uncrease the default timeout
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@4680 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-seam/ipbx/src/main/import.sql
+++ sip-servlets-seam/ipbx/src/main/import.sql
@@ -35,7 +35,7 @@ insert into ROLES(ROLEID,VERSION,USERID,role) values(5,1,3,'caller');
 insert into ROLES(ROLEID,VERSION,USERID,role) values(6,1,3,'admin');
 
 insert into GLOBALPROPERTIES(GLOBALPROPERTYID, VERSION, NAME, VALUE) values (1,1,'pbx.hostname','localhost')
-insert into GLOBALPROPERTIES(GLOBALPROPERTYID, VERSION, NAME, VALUE) values (2,1,'pbx.call.timeout','10000')
+insert into GLOBALPROPERTIES(GLOBALPROPERTYID, VERSION, NAME, VALUE) values (2,1,'pbx.call.timeout','80000')
 insert into GLOBALPROPERTIES(GLOBALPROPERTYID, VERSION, NAME, VALUE) values (3,1,'pbx.default.ringback.tone','http://mobicents.googlecode.com/svn/trunk/servers/sip-servlets/sip-servlets-seam/ipbx/audio/ringback.wav')
 insert into GLOBALPROPERTIES(GLOBALPROPERTYID, VERSION, NAME, VALUE) values (4,1,'pbx.default.rejected.announcement','rejected.wav')
 
