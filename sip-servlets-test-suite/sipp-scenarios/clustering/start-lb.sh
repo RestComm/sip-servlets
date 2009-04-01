44a6f813da882b2ba38a734d05d4b482d8136a39
jean.deruelle <jean.deruelle@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-04-01 12:57:36 +0000

    updating the scripts to latest sip lb
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@5269 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-test-suite/sipp-scenarios/clustering/start-lb.sh
+++ sip-servlets-test-suite/sipp-scenarios/clustering/start-lb.sh
@@ -1,2 +1,2 @@
 rm ./logs/*
-java -Djava.util.logging.config.file=./lb-logging.properties -jar ${M2_REPO}/org/mobicents/tools/sip-balancer/1.0-SNAPSHOT/sip-balancer-1.0-SNAPSHOT-jar-with-dependencies.jar lb-configuration.properties -Xms1024m -Xmx1024m -XX:PermSize=128M -XX:MaxPermSize=256M -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode
+java -Djava.util.logging.config.file=./lb-logging.properties -jar ${M2_REPO}/org/mobicents/tools/sip-balancer/1.0.BETA4-SNAPSHOT/sip-balancer-1.0.BETA4-SNAPSHOT-jar-with-dependencies.jar -mobicents-balancer-config=lb-configuration.properties -Xms1024m -Xmx1024m -XX:PermSize=128M -XX:MaxPermSize=256M -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode
