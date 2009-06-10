77888d0b8e643532e695781a1faaeb5a8afb05cf
vladimir.ralev <vladimir.ralev@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-06-10 21:43:43 +0000

    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@6547 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-eclipse-plugin/sip-phone-plugin/src/net/java/sip/communicator/impl/media/MediaGraphListener.java
+++
@@ -1,5 +0,0 @@
-package net.java.sip.communicator.impl.media;
-
-public interface MediaGraphListener {
-	public void update(int min, int max);
-}
--- sip-servlets-eclipse-plugin/sip-phone-plugin/src/org/mobicents/sip/phone/views/VisualizationService.java
+++
@@ -1,9 +0,0 @@
-package org.mobicents.sip.phone.views;
-
-import org.eclipse.swt.graphics.Point;
-
-public interface VisualizationService {
-
-	public abstract void addValue(Point value);
-
-}
