95f40e470955e69d2cfed0dafe1e58a96592c340
vladimir.ralev <vladimir.ralev@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-07-03 20:58:19 +0000

    Auto-cleanup before test start
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@6832 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-test-suite/sipp-scenarios/performance/b2bua/performance-b2bua-test.sh
+++ sip-servlets-test-suite/sipp-scenarios/performance/b2bua/performance-b2bua-test.sh
@@ -1,5 +1,5 @@
 #Don't forget to kill the sipp uas process after the test run it might still hang
-
+killall sipp
 ../sipp 127.0.0.1:5080 -sf call-forwarding-receiver.xml -trace_err -i 127.0.0.1 -p 5090 -bg
 ../sipp 127.0.0.1:5080 -s receiver -sf call-forwarding-sender.xml -trace_err -i 127.0.0.1 -p 5050 -r 80 -m 1000000
 sleep 10
