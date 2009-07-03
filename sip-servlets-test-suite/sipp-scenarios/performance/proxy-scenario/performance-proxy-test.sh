746c6c85caed57dbea0ce0c43d4a822dc1eeb39a
vladimir.ralev <vladimir.ralev@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-07-03 21:06:03 +0000

    Autocleanup before tests
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@6833 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-test-suite/sipp-scenarios/performance/proxy-scenario/performance-proxy-test.sh
+++ sip-servlets-test-suite/sipp-scenarios/performance/proxy-scenario/performance-proxy-test.sh
@@ -1,4 +1,5 @@
 #Don't forget to kill the sipp uas process after the test run it might still hang 
+killall sipp
 ../sipp 127.0.0.1:5080 -sf performance-proxy-uas.xml -trace_err -i 127.0.0.1 -p 5090 -bg
 ../sipp 127.0.0.1:5080 -sf performance-proxy-uac.xml -trace_err -i 127.0.0.1 -p 5055 -r 50 -m 100000
 sleep 10
