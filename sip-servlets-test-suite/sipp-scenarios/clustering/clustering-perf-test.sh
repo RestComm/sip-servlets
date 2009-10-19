3996c16b08e36959c634a3f54dfc7ce504496db3
jean.deruelle@gmail.com <jean.deruelle@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-10-19 09:02:30 +0000

    adding some HA perf tests setup
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@8082 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ sip-servlets-test-suite/sipp-scenarios/clustering/clustering-perf-test.sh
@@ -0,0 +1,61 @@
+#export TEST_IP=192.168.0.10
+export TEST_IP=127.0.0.1
+echo Using $TEST_IP for the test
+
+if [ $# -ne 0 ]; then
+	case $1 in	
+	    proxy)
+	    		rm ./proxy/*.log
+	    		echo "Distributed example used is proxy";
+	    		./sipp $TEST_IP:5080 -sf proxy/location-service-receiver.xml -i $TEST_IP -p 5090 -bg -trace_msg -timeout 30  
+	    		./sipp $TEST_IP:5080 -s receiver-failover -sf proxy/location-service-sender.xml -trace_err -i $TEST_IP -p 5050 -r 1 -m 1 -rsa $TEST_IP:5060 -trace_msg
+	            ;;
+	    b2bua)
+	    		rm ./b2bua/*.log
+	            echo "Distributed example used is b2bua";
+	            ./sipp $TEST_IP:5080 -sf b2bua/call-forwarding-receiver.xml -i $TEST_IP -p 5090 -bg -trace_msg -timeout 20  
+	    		./sipp $TEST_IP:5080 -s receiver -sf b2bua/call-forwarding-sender.xml -trace_err -i $TEST_IP -p 5050 -r 1 -m 1 -rsa $TEST_IP:5060 -trace_msg
+	            ;;
+	    b2bua-remote-send-bye)
+	    		rm ./b2bua/*.log
+	            echo "Distributed example used is b2bua-remote-send-bye";
+	            ./sipp $TEST_IP:5080 -sf b2bua/call-forwarding-receiver-sends-bye.xml -i $TEST_IP -p 5090 -bg -trace_msg -timeout 30 
+	    		./sipp $TEST_IP:5080 -s receiver -sf b2bua/call-forwarding-sender-receives-bye.xml -trace_err -i $TEST_IP -p 5050 -r 1 -m 1 -rsa $TEST_IP:5060 -trace_msg -nd
+	            ;;
+		uac)
+				rm ./uac/*.log
+	            echo "Distributed example used is uac";
+	            ./sipp $TEST_IP:5080 -sf uac/receiver.xml -i $TEST_IP -p 5090 -trace_msg -timeout 30
+	            ;;	          
+		c2c)
+				rm ./converged-click2call/*.log
+	            echo "Distributed example used is 3pcc";
+	            echo "Now, quickly navigate a browser to this URL: http://localhost/click2call-distributable/call?to=sip:to@$TEST_IP:5090&from=sip:from@$TEST_IP:5091";
+	            echo "Then, wait a few secs for the dialog to be established and kill the primary server. The secondary server will take over.";
+	            echo "40 secs after you have loaded the URL, one of the SIPP clients will send a BYE to close the dialog.";
+	            echo "If nothing happens, check if your apache server with mod_jk installed is running and configured properly.";
+	            echo "There are sample config files to run apache and mod_jkon localhost in the converged-click2call directory.";
+	            echo "If you need more info about how to configure mod_jk see http://www.jboss.org/community/docs/DOC-12525";
+                killall sipp
+	            ./sipp $TEST_IP:5080 -sf converged-click2call/receiver.xml -i $TEST_IP -p 5090 -trace_msg -timeout 100 -bg
+                ./sipp $TEST_IP:5080 -sf converged-click2call/receiver-sendbye.xml -i $TEST_IP -p 5091 -trace_msg -timeout 100 -bg
+                sleep 1
+                #wget "http://localhost/click2call-distributable/call?to=sip:to@$TEST_IP:5090&from=sip:from@$TEST_IP:5091"
+	            ;;
+	    uas-reinvite)
+	    		rm ./uas/*.log
+	            echo "Distributed example used is uas";
+	    		./sipp $TEST_IP:5080 -s isendbye -sf uas/clustering-reinvite-uac.xml -trace_err -i $TEST_IP -p 5050 -r 1 -m 1 -rsa $TEST_IP:5060 -trace_msg -nd
+	    		;;	            	   
+	    uas-timer)
+	    		rm ./uas/*.log
+	            echo "Distributed example used is uas";
+	    		./sipp $TEST_IP:5080 -s yousendbye -sf uas/clustering-uac-timer.xml -trace_err -i $TEST_IP -p 5050 -r 1 -m 1 -rsa $TEST_IP:5060 -trace_msg -nd
+	            ;;
+	    *)
+	    		rm ./uas/*.log
+	            echo "Distributed example used is uas";
+	    		./sipp $TEST_IP:5080 -s isendbye -sf uas/clustering-uac.xml -trace_err -i $TEST_IP -p 5050 -r 300 -m 100000 -trace_msg -nd
+	            ;;
+    esac
+fi
---
+++ sip-servlets-test-suite/sipp-scenarios/clustering/prepare-jboss-server-for-clustering-perf-one-node.sh
@@ -0,0 +1,73 @@
+#export JBOSS_HOME=/home/deruelle/tests/mss-1.0/jboss-4
+
+#echo "Script name is		[$0]"
+#echo "First Parameter is		[$1]"
+#echo "Second Parameter is		[$2]"
+#echo "This Process ID is		[$$]"
+#echo "This Parameter Count is	[$#]"
+#echo "All Parameters		[$@]"
+#echo "The FLAGS are			[$-]"
+
+
+rm -rf $JBOSS_HOME/server/port-1
+rm -rf $JBOSS_HOME/server/port-2
+
+mvn clean install -o -f ../../../pom.xml -P jboss,jboss-cluster -Dnode=all
+cp setup/jboss/performance/mss-sip-stack.properties $JBOSS_HOME/server/all/conf/mss-sip-stack.properties
+cp setup/jboss/jboss-context.xml $JBOSS_HOME/server/all/deploy/jboss-web.deployer/context.xml
+cp setup/jboss/jboss-tomcat-service.xml $JBOSS_HOME/server/all/deploy/jboss-web.deployer/META-INF/jboss-service.xml
+cp setup/jboss/webserver-xmbean.xml $JBOSS_HOME/server/all/deploy/jboss-web.deployer/META-INF/webserver-xmbean.xml
+cp setup/jboss/performance/log4j.xml $JBOSS_HOME/server/all/conf/jboss-log4j.xml
+cp setup/jboss/server-jboss-failover-all.xml $JBOSS_HOME/server/all/deploy/jboss-web.deployer/server.xml
+cp setup/jboss/jboss-service-all.xml $JBOSS_HOME/server/all/conf/jboss-service.xml
+mkdir $JBOSS_HOME/server/all/conf/dars
+
+cp -rf $JBOSS_HOME/server/all $JBOSS_HOME/server/port-1
+cp setup/jboss/jboss-service-port-1.xml $JBOSS_HOME/server/port-1/conf/jboss-service.xml
+cp setup/jboss/performance/server-jboss-failover-port-1.xml $JBOSS_HOME/server/port-1/deploy/jboss-web.deployer/server.xml
+
+if [ $# -ne 0 ]; then
+	case $1 in	
+	    proxy)
+	    		echo "Distributed example used is proxy"
+	    		mvn clean install -o -f ../../../sip-servlets-examples/location-service-distributable/pom.xml
+				cp ../../../sip-servlets-examples/location-service-distributable/target/location-service-distributable-*.war $JBOSS_HOME/server/port-1/deploy
+				cp ../../../sip-servlets-examples/location-service-distributable/target/location-service-distributable-*.war $JBOSS_HOME/server/port-2/deploy
+				cp ../../../sip-servlets-examples/location-service-distributable/distributable-location-service-dar.properties $JBOSS_HOME/server/port-1/conf/dars/distributable-dar.properties
+				cp ../../../sip-servlets-examples/location-service-distributable/distributable-location-service-dar.properties $JBOSS_HOME/server/port-2/conf/dars/distributable-dar.properties
+	            ;;
+	    b2bua)
+	            echo "Distributed example used is b2bua"
+	    		mvn clean install -o -f ../../../sip-servlets-examples/call-forwarding-distributable/pom.xml
+				cp ../../../sip-servlets-examples/call-forwarding-distributable/target/call-forwarding-distributable-*.war $JBOSS_HOME/server/port-1/deploy
+				cp ../../../sip-servlets-examples/call-forwarding-distributable/target/call-forwarding-distributable-*.war $JBOSS_HOME/server/port-2/deploy
+				cp ../../../sip-servlets-examples/call-forwarding-distributable/distributable-call-forwarding-dar.properties $JBOSS_HOME/server/port-1/conf/dars/distributable-dar.properties
+				cp ../../../sip-servlets-examples/call-forwarding-distributable/distributable-call-forwarding-dar.properties $JBOSS_HOME/server/port-2/conf/dars/distributable-dar.properties
+	            ;;
+	    c2c)
+	    		echo "Distributed example used is Click To call"
+	    		mvn clean install -o -f ../../../sip-servlets-examples/click2call-distributable/pom.xml
+				cp ../../../sip-servlets-examples/click2call-distributable/target/click2call-distributable*.war $JBOSS_HOME/server/port-1/deploy/click2call-distributable.war
+				cp ../../../sip-servlets-examples/click2call-distributable/target/click2call-distributable*.war $JBOSS_HOME/server/port-2/deploy/click2call-distributable.war
+				echo "" > $JBOSS_HOME/server/port-1/conf/dars/distributable-dar.properties
+				echo "" > $JBOSS_HOME/server/port-2/conf/dars/distributable-dar.properties
+				;;
+	    uac)
+	            echo "Distributed example used is uac"
+	    		mvn clean process-resources -Dsend.on.init=true install -o -f ../../../sip-servlets-examples/shootist-sip-servlet-distributable/pom.xml
+				cp ../../../sip-servlets-examples/shootist-sip-servlet-distributable/target/shootist-sip-servlet-distributable-*.war $JBOSS_HOME/server/port-1/deploy
+				mvn clean process-resources -Dsend.on.init=false install -o -f ../../../sip-servlets-examples/shootist-sip-servlet-distributable/pom.xml
+				cp ../../../sip-servlets-examples/shootist-sip-servlet-distributable/target/shootist-sip-servlet-distributable-*.war $JBOSS_HOME/server/port-2/deploy
+				cp ../../../sip-servlets-examples/shootist-sip-servlet-distributable/distributable-shootist-dar.properties $JBOSS_HOME/server/port-1/conf/dars/distributable-dar.properties
+				cp ../../../sip-servlets-examples/shootist-sip-servlet-distributable/distributable-shootist-dar.properties $JBOSS_HOME/server/port-2/conf/dars/distributable-dar.properties
+	            ;;
+	    *)
+	            echo "Distributed example used is uas"
+	    		mvn clean install -o -f ../../../sip-servlets-examples/simple-sip-servlet-distributable/pom.xml
+				cp ../../../sip-servlets-examples/simple-sip-servlet-distributable/target/simple-sip-servlet-distributable-*.war $JBOSS_HOME/server/port-1/deploy
+				cp ../../../sip-servlets-examples/simple-sip-servlet-distributable/target/simple-sip-servlet-distributable-*.war $JBOSS_HOME/server/port-2/deploy
+				cp ../../../sip-servlets-examples/simple-sip-servlet-distributable/distributable-simple-dar.properties $JBOSS_HOME/server/port-1/conf/dars/distributable-dar.properties
+				cp ../../../sip-servlets-examples/simple-sip-servlet-distributable/distributable-simple-dar.properties $JBOSS_HOME/server/port-2/conf/dars/distributable-dar.properties
+	            ;;
+    esac
+fi
