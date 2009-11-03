a35481d18b46bba77e0ac59caa7b9557daca7d6e
jean.deruelle@gmail.com <jean.deruelle@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-11-03 16:08:42 +0000

    fix scripts for proxy test
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@8240 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-test-suite/sipp-scenarios/performance/proxy-scenario/performance-proxy-test.sh
+++ sip-servlets-test-suite/sipp-scenarios/performance/proxy-scenario/performance-proxy-test.sh
@@ -1,5 +1,6 @@
 #Don't forget to kill the sipp uas process after the test run it might still hang 
 killall sipp
-../sipp 127.0.0.1:5080 -sf performance-proxy-uas.xml -trace_err -i 127.0.0.1 -p 5090 -bg
-../sipp 127.0.0.1:5080 -sf performance-proxy-uac.xml -trace_err -i 127.0.0.1 -p 5055 -r 50 -m 100000
+rm *.log
+../sipp 127.0.0.1:5055 -sf performance-proxy-uas.xml -trace_err -trace_msg -i 127.0.0.1 -p 5090 -bg
+../sipp 127.0.0.1:5090 -sf performance-proxy-uac.xml -trace_err -trace_msg -i 127.0.0.1 -p 5055 -rsa 127.0.0.1:5080 -r 1 -m 1
 sleep 10
--- sip-servlets-test-suite/sipp-scenarios/performance/proxy-scenario/prepare-jboss-server-for-proxy-perf.sh
+++ sip-servlets-test-suite/sipp-scenarios/performance/proxy-scenario/prepare-jboss-server-for-proxy-perf.sh
@@ -4,6 +4,7 @@ cp ../jboss-setup/jboss-service.xml $JBOSS_HOME/server/default/deploy/jboss-web.
 cp ../jboss-setup/webserver-xmbean.xml $JBOSS_HOME/server/default/deploy/jboss-web.deployer/META-INF/webserver-xmbean.xml
 cp ../jboss-setup/log4j.xml $JBOSS_HOME/server/default/conf/jboss-log4j.xml
 cp ../mss-sip-stack.properties $JBOSS_HOME/server/default/conf/mss-sip-stack.properties
+mvn clean install -f ../../../../pom.xml -P jboss
 mvn clean install -f ../../../../sip-servlets-examples/location-service/pom.xml
 cp ../../../../sip-servlets-examples/location-service/target/location-service-*.war $JBOSS_HOME/server/default/deploy
 cp ../../../../sip-servlets-examples/location-service/locationservice-dar.properties $JBOSS_HOME/server/default/conf/dars/mobicents-dar.properties
