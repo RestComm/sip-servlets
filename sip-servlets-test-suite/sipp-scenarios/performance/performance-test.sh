2a7d38c568c25f0bfa42cfbc3c1e94f5ff27af1b
jean.deruelle@gmail.com <jean.deruelle@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-11-19 10:43:38 +0000

    fix to scripts for testing performance of different use cases
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@8569 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-test-suite/sipp-scenarios/performance/performance-test.sh
+++ sip-servlets-test-suite/sipp-scenarios/performance/performance-test.sh
@@ -1,6 +1,6 @@
 killall sipp
 rm *.log
-#export TEST_IP=192.168.0.13
+#export TEST_IP=192.168.0.12
 export TEST_IP=127.0.0.1
 echo "IP Address for the test is $TEST_IP"
 
@@ -9,21 +9,21 @@ if [ $# -ne 0 ]; then
 	    proxy)
 	    		rm ./proxy/*.log
 	    		echo "Distributed example used is proxy";
-	    		../sipp $TEST_IP:5055 -sf proxy-scenario/performance-proxy-uas.xml -trace_err -trace_msg -i $TEST_IP -p 5090 -bg
-				../sipp $TEST_IP:5090 -sf proxy-scenario/performance-proxy-uac.xml -trace_err -trace_msg -i $TEST_IP -p 5055 -rsa $TEST_IP:5080 -r 1 -m 1
+	    		./sipp $TEST_IP:5055 -sf proxy-scenario/performance-proxy-uas.xml -trace_err -trace_msg -i $TEST_IP -p 5090 -bg
+				./sipp $TEST_IP:5090 -sf proxy-scenario/performance-proxy-uac.xml -trace_err -trace_msg -i $TEST_IP -p 5055 -rsa $TEST_IP:5080 -r 1 -m 1
 				sleep 10
 	            ;;
 	    b2bua)
 	    		rm ./b2bua/*.log
 	            echo "Distributed example used is b2bua";
-	            ../sipp $TEST_IP:5080 -sf b2bua/call-forwarding-receiver.xml -trace_err -i $TEST_IP -p 5090 -bg
-				../sipp $TEST_IP:5080 -s receiver -sf b2bua/call-forwarding-sender.xml -trace_err -i $TEST_IP -p 5050 -r 1 -m 1
+	            ./sipp $TEST_IP:5080 -sf b2bua/call-forwarding-receiver.xml -trace_err -i $TEST_IP -p 5090 -bg
+				./sipp $TEST_IP:5080 -s receiver -sf b2bua/call-forwarding-sender.xml -trace_err -i $TEST_IP -p 5050 -r 5 -m 50
 				sleep 10
 	            ;;
 	    *)
 	    		rm ./uas/*.log
 	            echo "Distributed example used is uas";	    		
-	    		./sipp $TEST_IP:5080 -s yousendbye -sf simple-flow/performance-uac.xml -trace_err -i $TEST_IP -p 5055 -r 1 -m 1
+	    		./sipp $TEST_IP:5080 -s yousendbye -sf simple-flow/performance-uac.xml -trace_err -i $TEST_IP -p 5055 -r 5 -m 50
 	            ;;
     esac
 fi
