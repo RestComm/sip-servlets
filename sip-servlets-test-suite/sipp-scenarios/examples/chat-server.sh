1ca3b44b311fcdab3d4b5ec6a67a936a2bb8a673
jean.deruelle@gmail.com <jean.deruelle@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-11-19 10:43:02 +0000

    fix to scripts for testing examples
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@8568 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-test-suite/sipp-scenarios/examples/chat-server.sh
+++ sip-servlets-test-suite/sipp-scenarios/examples/chat-server.sh
@@ -1,3 +1,4 @@
+rm -rf chat-server/*.log
 ./sipp 127.0.0.1:5080 -sf chat-server/chat-server-a.xml -i 127.0.0.1 -p 5055 -r 1 -m 1 -bg -trace_msg
 sleep 2
 ./sipp 127.0.0.1:5080 -sf chat-server/chat-server-b.xml -i 127.0.0.1 -p 5055 -r 1 -m 1 -bg -trace_msg
