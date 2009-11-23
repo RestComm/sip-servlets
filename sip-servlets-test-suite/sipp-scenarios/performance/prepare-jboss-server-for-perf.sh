da96c1de6778d1bc1486f02d28acd607067f077d
jean.deruelle <jean.deruelle@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-11-23 11:32:23 +0000

    script fix
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@8630 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-test-suite/sipp-scenarios/performance/prepare-jboss-server-for-perf.sh
+++ sip-servlets-test-suite/sipp-scenarios/performance/prepare-jboss-server-for-perf.sh
@@ -1,10 +1,10 @@
-mvn clean install -f ../../../pom.xml -P jboss -U
+#mvn clean install -f ../../../pom.xml -P jboss -U
 
 cp jboss-setup/mss-sip-stack.properties $JBOSS_HOME/server/default/conf/mss-sip-stack.properties
 cp jboss-setup/server-jboss.xml $JBOSS_HOME/server/default/deploy/jboss-web.deployer/server.xml
-cp jboss-setup/jboss-context.xml $JBOSS_HOME/server/default/deploy/jboss-web.deployer/context.xml
-cp jboss-setup/jboss-service.xml $JBOSS_HOME/server/default/deploy/jboss-web.deployer/META-INF/jboss-service.xml
-cp jboss-setup/webserver-xmbean.xml $JBOSS_HOME/server/default/deploy/jboss-web.deployer/META-INF/webserver-xmbean.xml
+#cp jboss-setup/jboss-context.xml $JBOSS_HOME/server/default/deploy/jboss-web.deployer/context.xml
+#cp jboss-setup/jboss-service.xml $JBOSS_HOME/server/default/deploy/jboss-web.deployer/META-INF/jboss-service.xml
+#cp jboss-setup/webserver-xmbean.xml $JBOSS_HOME/server/default/deploy/jboss-web.deployer/META-INF/webserver-xmbean.xml
 cp jboss-setup/log4j.xml $JBOSS_HOME/server/default/conf/jboss-log4j.xml
 
 if [ $# -ne 0 ]; then
@@ -12,7 +12,7 @@ if [ $# -ne 0 ]; then
 	    proxy)
 	    		echo "Distributed example used is proxy"
 	    		mvn clean install -o -f ../../../sip-servlets-examples/location-service/pom.xml
-				cp ../../../sip-servlets-examples/location-service/target/location-service-distributable-*.war $JBOSS_HOME/server/default/deploy
+				cp ../../../sip-servlets-examples/location-service/target/location-service-*.war $JBOSS_HOME/server/default/deploy
 				cp ../../../sip-servlets-examples/location-service/locationservice-dar.properties $JBOSS_HOME/server/default/conf/dars/mobicents-dar.properties
 	            ;;
 	    b2bua)
