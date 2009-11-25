f790edd066ee8edd8a6d6264a3fb4907a4fa4fb0
lbbbarreiro <lbbbarreiro@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-11-25 16:46:24 +0000

    Fix sipp script
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@8649 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-test-suite/sipp-scenarios/examples/download-and-compile-sipp.sh
+++ sip-servlets-test-suite/sipp-scenarios/examples/download-and-compile-sipp.sh
@@ -1,16 +1,5 @@
-# Compile sipp
-wget -m -nd http://superb-east.dl.sourceforge.net/sourceforge/sipp/sipp-3.0.src.tar.gz
-tar -xzf sipp-3.0.src.tar.gz
-cd sipp-3.0.src
-
-wget -m -nd http://ftp.gnu.org/pub/gnu/ncurses/ncurses-5.6.tar.gz
-tar -xzf ncurses-5.6.tar.gz
-cd ncurses-5.6
-./configure > /dev/null
-make > /dev/null
-cd ..
-wget -m -nd http://people.redhat.com/lbarreir/hudson/call-setup-test/Makefile
-make > /dev/null
-mv -f sipp ..
-cd ..
-rm -fr  sipp-3.0.src*
+wget http://sipp.sourceforge.net/snapshots/sipp.2009-07-29.tar.gz
+tar -xzf sipp.*.tar.gz
+make -C sipp.svn > /dev/null
+mv -f sipp.svn/sipp .
+rm -fr sipp.*.tar.gz sipp.svn
