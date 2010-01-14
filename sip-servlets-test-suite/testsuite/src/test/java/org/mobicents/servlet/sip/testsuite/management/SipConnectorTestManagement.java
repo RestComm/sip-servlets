a64fa6794035a638cacbd295ac3b865f1f93ce67
jean.deruelle <jean.deruelle@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-01-14 16:04:11 +0000

    Update issue 1163
    Non regression test added, couldn't reproduce, could you attach your log file ? 
    
    
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@9479 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-test-suite/testsuite/src/test/java/org/mobicents/servlet/sip/testsuite/management/SipConnectorTestManagement.java
+++ sip-servlets-test-suite/testsuite/src/test/java/org/mobicents/servlet/sip/testsuite/management/SipConnectorTestManagement.java
@@ -53,8 +53,11 @@ public class SipConnectorTestManagement extends SipServletTestCase {
 		"sipConnectorRemoved",
 	};	
 	
-	TestSipListener sender;
-	ProtocolObjects senderProtocolObjects;	
+	TestSipListener receiver;
+	ProtocolObjects receiverProtocolObjects;
+	
+	TestSipListener receiverTcp;
+	ProtocolObjects receiverTcpProtocolObjects;	
 	
 	public SipConnectorTestManagement(String name) {
 		super(name);
@@ -81,13 +84,21 @@ public class SipConnectorTestManagement extends SipServletTestCase {
 	protected void setUp() throws Exception {
 		super.setUp();						
 		
-		senderProtocolObjects =new ProtocolObjects(
-				"sender", "gov.nist", TRANSPORT, AUTODIALOG, null);
+		receiverProtocolObjects =new ProtocolObjects(
+				"receiver", "gov.nist", TRANSPORT, AUTODIALOG, null);
 		
-		sender = new TestSipListener(5080, 5070, senderProtocolObjects, true);
-		SipProvider senderProvider = sender.createProvider();			
-		senderProvider.addSipListener(sender);
-		senderProtocolObjects.start();		
+		receiver = new TestSipListener(5080, 5070, receiverProtocolObjects, true);
+		SipProvider receiverProvider = receiver.createProvider();			
+		receiverProvider.addSipListener(receiver);
+		receiverProtocolObjects.start();
+		
+		receiverTcpProtocolObjects =new ProtocolObjects(
+				"receiverTcp", "gov.nist", ListeningPoint.TCP, AUTODIALOG, null);
+		
+		receiverTcp = new TestSipListener(5081, 5070, receiverTcpProtocolObjects, true);
+		SipProvider receiverTcpProvider = receiverTcp.createProvider();			
+		receiverTcpProvider.addSipListener(receiverTcp);
+		receiverTcpProtocolObjects.start();		
 	}
 	
 	public void testShootme() throws Exception {
@@ -138,20 +149,103 @@ public class SipConnectorTestManagement extends SipServletTestCase {
         
         Thread.sleep(TIMEOUT);
         
-        Iterator<String> allMessagesIterator = sender.getAllMessagesContent().iterator();
+        Iterator<String> allMessagesIterator = receiver.getAllMessagesContent().iterator();
 		while (allMessagesIterator.hasNext()) {
 			String message = (String) allMessagesIterator.next();
 			logger.info(message);
 		}
-		assertEquals(3, sender.getAllMessagesContent().size());
+		assertEquals(3, receiver.getAllMessagesContent().size());
 		for (int i = 0; i < CONNECTORS_TO_TEST.length; i++) {
-			assertTrue(sender.getAllMessagesContent().contains(CONNECTORS_TO_TEST[i]));
+			assertTrue(receiver.getAllMessagesContent().contains(CONNECTORS_TO_TEST[i]));
 		}	
+	}
+	
+	/**
+	 * Non regression  test for Issue 1161 : Only the last sip connector (which was added programmatically using JMX) is working
+	 * http://code.google.com/p/mobicents/issues/detail?id=1161
+	 */
+	public void testShootmeTCPAndUDP() throws Exception {
+		logger.info("\nCreate an RMI connector client and " +
+		 "connect it to the RMI connector server");
+		StringBuffer urlString = new StringBuffer();
+		urlString.append("service:jmx:rmi://localhost:");
+		urlString.append(8999);
+		urlString.append("/jndi/rmi://localhost:");
+		urlString.append(8999);
+		urlString.append("/jmxrmi");
+           JMXServiceURL url = new JMXServiceURL(
+        		   urlString.toString());
+           JMXConnector jmxc = JMXConnectorFactory.connect(url, null);
+
+           
+		ObjectName objectName = new ObjectName(serverName + ":type=Service,serviceName="+ serverName);
+		// Get the Platform MBean Server
+		MBeanServerConnection mbsc = jmxc.getMBeanServerConnection();
+        MBeanInfo mbeanInfo = mbsc.getMBeanInfo(objectName);
+        MBeanOperationInfo[] operationInfos = mbeanInfo.getOperations();
+        System.out.println("MBean Operations:");
+        String[] operationNames = new String[operationInfos.length];
+        for (int i = 0; i < operationInfos.length; i++) {
+           System.out.println(i + ": " + operationInfos[i].getDescription() + " " +
+        		   operationInfos[i].getSignature());
+           operationNames[i] = operationInfos[i].getName();
+        }
+		
+        SipConnector[] sipConnectors = (SipConnector[]) mbsc.invoke(objectName, "findSipConnectors", null, null);
+//        Thread.sleep(TIMEOUT);
+        assertEquals(1, sipConnectors.length);
+        for (int i = 0; i < sipConnectors.length; i++) {
+			logger.info(sipConnectors[i]);
+		}
+        
+        mbsc.invoke(objectName, "removeSipConnector",new Object[] {"127.0.0.1", 5070, ListeningPoint.UDP}, new String[]{String.class.getCanonicalName(), int.class.getCanonicalName(), String.class.getCanonicalName()});
+        sipConnectors = (SipConnector[]) mbsc.invoke(objectName, "findSipConnectors", null, null);
+        assertEquals(0, sipConnectors.length);     
+        
+        Thread.sleep(TIMEOUT);
+        
+        receiver.getAllMessagesContent().clear();
+        receiverTcp.getAllMessagesContent().clear();
+        
+        // adding udp connector
+        SipConnector udpSipConnector = new SipConnector();
+        udpSipConnector.setIpAddress("127.0.0.1");
+        udpSipConnector.setPort(5071);
+        udpSipConnector.setTransport(ListeningPoint.UDP);
+        mbsc.invoke(objectName, "addSipConnector",new Object[] {udpSipConnector}, new String[]{SipConnector.class.getCanonicalName()});
+        // adding tcp connector
+        SipConnector tcpSipConnector = new SipConnector();
+        tcpSipConnector.setIpAddress("127.0.0.1");
+        tcpSipConnector.setPort(5071);
+        tcpSipConnector.setTransport(ListeningPoint.TCP);
+        mbsc.invoke(objectName, "addSipConnector",new Object[] {tcpSipConnector}, new String[]{SipConnector.class.getCanonicalName()});
+        // making sure they were both added correctly
+        sipConnectors = (SipConnector[]) mbsc.invoke(objectName, "findSipConnectors", null, null);
+        assertEquals(2, sipConnectors.length);
+        
+        Thread.sleep(TIMEOUT);
+        
+        Iterator<String> allMessagesIterator = receiver.getAllMessagesContent().iterator();
+		while (allMessagesIterator.hasNext()) {
+			String message = (String) allMessagesIterator.next();
+			logger.info(message);
+		}
+		assertEquals(1, receiver.getAllMessagesContent().size());
+		assertTrue(receiver.getAllMessagesContent().contains("sipConnectorAdded"));
+		
+		allMessagesIterator = receiverTcp.getAllMessagesContent().iterator();
+		while (allMessagesIterator.hasNext()) {
+			String message = (String) allMessagesIterator.next();
+			logger.info(message);
+		}
+		assertEquals(1, receiverTcp.getAllMessagesContent().size());
+		assertTrue(receiverTcp.getAllMessagesContent().contains("sipConnectorAdded"));                
 	}
 	
 	@Override
 	protected void tearDown() throws Exception {					
-		senderProtocolObjects.destroy();	
+		receiverProtocolObjects.destroy();	
+		receiverTcpProtocolObjects.destroy();
 		logger.info("Test completed");
 		super.tearDown();
 	}
