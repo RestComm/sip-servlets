cd12bc81d074c2e3339a2f00bf90ae75b72c5cfa
jean.deruelle <jean.deruelle@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-01-28 10:42:00 +0000

    updating the perf tests script
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@9781 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-test-suite/sipp-scenarios/performance/performance-test.sh
+++ sip-servlets-test-suite/sipp-scenarios/performance/performance-test.sh
@@ -10,20 +10,20 @@ if [ $# -ne 0 ]; then
 	    		rm ./proxy/*.log
 	    		echo "Distributed example used is proxy";
 	    		./sipp $TEST_IP:5055 -sf proxy-scenario/performance-proxy-uas.xml -trace_err -i $TEST_IP -p 5090 -bg -nd
-				./sipp $TEST_IP:5090 -sf proxy-scenario/performance-proxy-uac.xml -trace_err -i $TEST_IP -p 5055 -rsa $TEST_IP:5080 -r 100 -m 1000000 -nd
+				./sipp $TEST_IP:5090 -sf proxy-scenario/performance-proxy-uac.xml -trace_err -i $TEST_IP -p 5055 -rsa $TEST_IP:5080 -r 100 -m 250000 -nd
 				sleep 10
 	            ;;
 	    b2bua)
 	    		rm ./b2bua/*.log
 	            echo "Distributed example used is b2bua";
 	            ./sipp $TEST_IP:5080 -sf b2bua/call-forwarding-receiver.xml -trace_err -i $TEST_IP -p 5090 -bg
-				./sipp $TEST_IP:5080 -s receiver -sf b2bua/call-forwarding-sender.xml -trace_err -i $TEST_IP -p 5050 -r 50 -m 1000000
+				./sipp $TEST_IP:5080 -s receiver -sf b2bua/call-forwarding-sender.xml -trace_err -i $TEST_IP -p 5050 -r 100 -m 250000
 				sleep 10
 	            ;;
 	    *)
 	    		rm ./uas/*.log
 	 			echo "Distributed example used is uas";	    		
-	    		./sipp $TEST_IP:5080 -s yousendbye -sf simple-flow/performance-uac.xml -trace_err -i $TEST_IP -p 5055 -r 100 -m 1000000
+	    		./sipp $TEST_IP:5080 -s yousendbye -sf simple-flow/performance-uac.xml -trace_err -i $TEST_IP -p 5055 -r 200 -m 250000
 	            ;;
     esac
 fi
--- sip-servlets-test-suite/sipp-scenarios/performance/start-jboss-server-for-perf.sh
+++ sip-servlets-test-suite/sipp-scenarios/performance/start-jboss-server-for-perf.sh
@@ -1,2 +1,2 @@
-export JAVA_OPTS="-Xms1536m -Xmx1536m -XX:PermSize=128M -XX:MaxPermSize=256M -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode"
+export JAVA_OPTS="-server -Xms2048m -Xmx2048m -XX:PermSize=128M -XX:MaxPermSize=256M -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode"
 sh $JBOSS_HOME/bin/run.sh run
