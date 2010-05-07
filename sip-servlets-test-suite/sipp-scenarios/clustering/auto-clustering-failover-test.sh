a69eac03d92ef54f752a213159453301a601af69
jean.deruelle <jean.deruelle@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-05-07 08:49:23 +0000

    removing timer tests from jb4 config since it is available on as5 only
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@11591 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-test-suite/sipp-scenarios/clustering/auto-clustering-failover-test.sh
+++ sip-servlets-test-suite/sipp-scenarios/clustering/auto-clustering-failover-test.sh
@@ -165,24 +165,9 @@ sleep $HALFSTARTSLEEP
 
 ./auto-run-test.sh uas-reinvite result.txt
 
-#The test killed server 1, so we start it again
-./auto-start-jboss-server.sh $config1 $config1.pid 0 uas-timer
-
-sleep $HALFSTARTSLEEP
-
-./auto-run-test.sh uas-timer result.txt
-
-#The test killed server 1, so we start it again
-./auto-start-jboss-server.sh $config1 $config1.pid 0 uas-sas-timer
-
-sleep $HALFSTARTSLEEP
-
-./auto-run-test.sh uas-sas-timer result.txt
-
-
 #Kill the app servers
-./auto-kill-process-tree.sh `cat $config1.pid` $config1
 ./auto-kill-process-tree.sh `cat $config2.pid` $config2
+./auto-kill-process-tree.sh `cat $config1.pid` $config1
 
 sleep 10
 
