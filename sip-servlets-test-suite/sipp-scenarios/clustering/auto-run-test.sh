f65aa06b58762e4c851e351194b3d0061d4c8730
vladimir.ralev <vladimir.ralev@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-05-10 14:42:17 +0000

    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@11647 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-test-suite/sipp-scenarios/clustering/auto-run-test.sh
+++ sip-servlets-test-suite/sipp-scenarios/clustering/auto-run-test.sh
@@ -6,3 +6,5 @@ export CURRENT_CODE=$?
 echo "$1 $CURRENT_CODE" >> $2
 
 ./auto-kill-process-tree.sh `cat $config1.pid` $config1
+cp $JBOSS_HOME/server/$config1/log/server.log ./$1-$config1-server.log
+cp $JBOSS_HOME/server/$config1/log/server.log ./$1-$config2-server.log
