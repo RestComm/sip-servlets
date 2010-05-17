1f9ac2c67868fa4c9cf7c203915eadc8617fb6b1
vladimir.ralev <vladimir.ralev@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-05-17 01:29:40 +0000

    Update Issue 1454 Test case
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@11831 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ sip-servlets-test-suite/testsuite/src/test/java/org/mobicents/servlet/sip/testsuite/composition/SpeedDialLocationServiceStaticServerAddressTest.java
@@ -0,0 +1,289 @@
+/*
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.mobicents.servlet.sip.testsuite.composition;
+
+import javax.sip.SipProvider;
+import javax.sip.address.SipURI;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.log4j.Logger;
+import org.mobicents.servlet.sip.SipServletTestCase;
+import org.mobicents.servlet.sip.startup.SipProtocolHandler;
+import org.mobicents.servlet.sip.testsuite.ProtocolObjects;
+import org.mobicents.servlet.sip.testsuite.TestSipListener;
+
+public class SpeedDialLocationServiceStaticServerAddressTest extends SipServletTestCase {
+	
+	private static transient Logger logger = Logger.getLogger(SpeedDialLocationServiceStaticServerAddressTest.class);
+
+	private static final String TRANSPORT = "udp";
+	private static final boolean AUTODIALOG = true;
+	private static final int TIMEOUT = 10000;	
+//	private static final int TIMEOUT = 100000000;
+	 
+	TestSipListener sender;
+	TestSipListener receiver;
+	ProtocolObjects senderProtocolObjects;
+	ProtocolObjects	receiverProtocolObjects;
+
+	public SpeedDialLocationServiceStaticServerAddressTest(String name) {
+		super(name);
+		startTomcatOnStartup = false;
+		addSipConnectorOnStartup =  false;
+		autoDeployOnStartup = false;
+	}
+
+	@Override
+	public void deployApplication() {
+		deploySpeedDial();
+		deployLocationService();
+	}
+
+	private void deploySpeedDial() {
+		assertTrue(tomcat.deployContext(
+				projectHome + "/sip-servlets-test-suite/applications/speed-dial-servlet/src/main/sipapp",
+				"speed-dial-context", 
+				"speed-dial"));
+	}
+	
+	private void deployLocationService() {
+		assertTrue(tomcat.deployContext(
+				projectHome + "/sip-servlets-test-suite/applications/location-service-servlet/src/main/sipapp",
+				"location-service-context", 
+				"location-service"));
+	}
+	
+	@Override
+	protected String getDarConfigurationFile() {
+		return "file:///"
+				+ projectHome
+				+ "/sip-servlets-test-suite/testsuite/src/test/resources/"
+				+ "org/mobicents/servlet/sip/testsuite/composition/speeddial-locationservice-dar.properties";
+	}
+	
+	@Override
+	protected void setUp() throws Exception {		
+		super.setUp();
+
+		Connector udpSipConnector = null;
+		try {
+			udpSipConnector = new Connector(
+					SipProtocolHandler.class.getName());
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		SipProtocolHandler udpProtocolHandler = (SipProtocolHandler) udpSipConnector
+				.getProtocolHandler();
+		try {
+			udpProtocolHandler.setPort(5070);
+			udpProtocolHandler.setIpAddress("127.0.0.1");
+
+			udpProtocolHandler.setSignalingTransport("udp");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		udpProtocolHandler.setSipStackProperties(null);
+		udpProtocolHandler.setUseStaticAddress(true);
+		udpProtocolHandler.setStaticServerAddress("127.0.0.1");
+		udpProtocolHandler.setStaticServerPort(33);
+		tomcat.getSipService().addConnector(udpSipConnector);
+		try {
+			tomcat.startTomcat();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		
+		deployApplication();
+
+		senderProtocolObjects = new ProtocolObjects("sender",
+				"gov.nist", TRANSPORT, AUTODIALOG, null);
+		receiverProtocolObjects = new ProtocolObjects("receiver",
+				"gov.nist", TRANSPORT, AUTODIALOG, null);			
+	}
+	
+	public void testSpeedDialLocationServiceCallerSendBye() throws Exception {		
+		sender = new TestSipListener(5080, 5070, senderProtocolObjects, true);
+		sender.setRecordRoutingProxyTesting(true);
+		SipProvider senderProvider = sender.createProvider();
+
+		receiver = new TestSipListener(5090, 5070, receiverProtocolObjects, false);
+		receiver.setRecordRoutingProxyTesting(true);
+		SipProvider receiverProvider = receiver.createProvider();
+
+		receiverProvider.addSipListener(receiver);
+		senderProvider.addSipListener(sender);
+
+		senderProtocolObjects.start();
+		receiverProtocolObjects.start();
+
+		String fromName = "sender";
+		String fromHost = "sip-servlets.com";
+		SipURI fromAddress = senderProtocolObjects.addressFactory.createSipURI(
+				fromName, fromHost);
+				
+		String toUser = "1";
+		String toHost = "sip-servlets.com";
+		SipURI toAddress = senderProtocolObjects.addressFactory.createSipURI(
+				toUser, toHost);
+		
+		sender.sendSipRequest("INVITE", fromAddress, toAddress, null, null, false);		
+		Thread.sleep(TIMEOUT);
+		assertTrue(sender.getOkToByeReceived());
+		assertTrue(receiver.getByeReceived());
+	}
+	
+	public void testSpeedDialLocationServiceErrorResponse() throws Exception {		
+		sender = new TestSipListener(5080, 5070, senderProtocolObjects, true);
+		sender.setRecordRoutingProxyTesting(true);
+		SipProvider senderProvider = sender.createProvider();
+
+		receiver = new TestSipListener(5090, 5070, receiverProtocolObjects, false);
+		receiver.setRecordRoutingProxyTesting(true);
+		SipProvider receiverProvider = receiver.createProvider();
+
+		receiverProvider.addSipListener(receiver);
+		senderProvider.addSipListener(sender);
+		receiver.setRespondWithError(408);
+		senderProtocolObjects.start();
+		receiverProtocolObjects.start();
+
+		String fromName = "sender-expect-408";
+		String fromHost = "sip-servlets.com";
+		SipURI fromAddress = senderProtocolObjects.addressFactory.createSipURI(
+				fromName, fromHost);
+				
+		String toUser = "1";
+		String toHost = "sip-servlets.com";
+		SipURI toAddress = senderProtocolObjects.addressFactory.createSipURI(
+				toUser, toHost);
+		
+		sender.sendSipRequest("INVITE", fromAddress, toAddress, null, null, false);		
+		Thread.sleep(TIMEOUT);
+		assertTrue(sender.isServerErrorReceived());
+	}
+
+	public void testSpeedDialLocationServiceCalleeSendBye() throws Exception {
+		sender = new TestSipListener(5080, 5070, senderProtocolObjects, false);
+		sender.setRecordRoutingProxyTesting(true);
+		SipProvider senderProvider = sender.createProvider();
+
+		receiver = new TestSipListener(5090, 5070, receiverProtocolObjects, true);
+		receiver.setRecordRoutingProxyTesting(true);
+		SipProvider receiverProvider = receiver.createProvider();
+
+		receiverProvider.addSipListener(receiver);
+		senderProvider.addSipListener(sender);
+
+		senderProtocolObjects.start();
+		receiverProtocolObjects.start();
+
+		String fromName = "sender";
+		String fromHost = "sip-servlets.com";
+		SipURI fromAddress = senderProtocolObjects.addressFactory.createSipURI(
+				fromName, fromHost);
+				
+		String toUser = "1";
+		String toHost = "sip-servlets.com";
+		SipURI toAddress = senderProtocolObjects.addressFactory.createSipURI(
+				toUser, toHost);
+		
+		sender.sendSipRequest("INVITE", fromAddress, toAddress, null, null, false);		
+		Thread.sleep(TIMEOUT);
+		assertTrue(receiver.getOkToByeReceived());
+		assertTrue(sender.getByeReceived());		
+	}
+
+	public void testCancelSpeedDialLocationService() throws Exception {
+		sender = new TestSipListener(5080, 5070, senderProtocolObjects, false);
+		sender.setRecordRoutingProxyTesting(true);
+		SipProvider senderProvider = sender.createProvider();
+
+		receiver = new TestSipListener(5090, 5070, receiverProtocolObjects, true);
+		receiver.setRecordRoutingProxyTesting(true);
+		receiver.setWaitForCancel(true);
+		SipProvider receiverProvider = receiver.createProvider();
+
+		receiverProvider.addSipListener(receiver);
+		senderProvider.addSipListener(sender);
+
+		senderProtocolObjects.start();
+		receiverProtocolObjects.start();
+
+		String fromName = "sender";
+		String fromHost = "sip-servlets.com";
+		SipURI fromAddress = senderProtocolObjects.addressFactory.createSipURI(
+				fromName, fromHost);
+				
+		String toUser = "1";
+		String toHost = "sip-servlets.com";
+		SipURI toAddress = senderProtocolObjects.addressFactory.createSipURI(
+				toUser, toHost);
+		
+		sender.sendSipRequest("INVITE", fromAddress, toAddress, null, null, false);
+		Thread.sleep(500);
+		sender.sendCancel();
+		Thread.sleep(TIMEOUT);
+		assertTrue(sender.isCancelOkReceived());
+		assertTrue(sender.isRequestTerminatedReceived());
+		assertTrue(receiver.isCancelReceived());
+	}
+
+	public void testRemoteAddrPortAndTransport() throws Exception {		
+		sender = new TestSipListener(5080, 5070, senderProtocolObjects, true);
+		sender.setRecordRoutingProxyTesting(true);
+		SipProvider senderProvider = sender.createProvider();
+
+		receiver = new TestSipListener(5090, 5070, receiverProtocolObjects, false);
+		receiver.setRecordRoutingProxyTesting(true);
+		SipProvider receiverProvider = receiver.createProvider();
+
+		receiverProvider.addSipListener(receiver);
+		senderProvider.addSipListener(sender);
+
+		senderProtocolObjects.start();
+		receiverProtocolObjects.start();
+
+		String fromName = "remote";
+		String fromHost = "sip-servlets.com";
+		SipURI fromAddress = senderProtocolObjects.addressFactory.createSipURI(
+				fromName, fromHost);
+				
+		String toUser = "1";
+		String toHost = "sip-servlets.com";
+		SipURI toAddress = senderProtocolObjects.addressFactory.createSipURI(
+				toUser, toHost);
+		
+		sender.sendSipRequest("INVITE", fromAddress, toAddress, null, null, false);		
+		Thread.sleep(TIMEOUT);
+		assertTrue(sender.getOkToByeReceived());
+		assertTrue(receiver.getByeReceived());
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {	
+		senderProtocolObjects.destroy();
+		receiverProtocolObjects.destroy();			
+		logger.info("Test completed");
+		super.tearDown();
+	}
+
+
+}
