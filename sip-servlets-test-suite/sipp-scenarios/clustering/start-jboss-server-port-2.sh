d506e140128710c49b8755d25266d733b00de32f
jean.deruelle <jean.deruelle@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-07-13 11:18:05 +0000

    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@12899 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-test-suite/sipp-scenarios/clustering/start-jboss-server-port-2.sh
+++ sip-servlets-test-suite/sipp-scenarios/clustering/start-jboss-server-port-2.sh
@@ -1,3 +1,3 @@
 #export JBOSS_HOME=/home/deruelle/servers/jboss-4.2.2.GA-cluster
 export JAVA_OPTS="-Xms1536m -Xmx1536m -XX:PermSize=128M -XX:MaxPermSize=256M -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode"
-sh $JBOSS_HOME/bin/run.sh -c port-2 -Djboss.partition.name=MSSPartition
+sh $JBOSS_HOME/bin/run.sh -c port-2
