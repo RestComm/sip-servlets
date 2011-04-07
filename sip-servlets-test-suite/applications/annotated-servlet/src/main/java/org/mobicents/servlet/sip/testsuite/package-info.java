0e67935aeb63f9e0487797d292f8bb150cf4e3d1
jean.deruelle@gmail.com <jean.deruelle@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2011-04-07 13:41:32 +0000

    Fixing classpath
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@17796 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-test-suite/applications/annotated-servlet/src/main/java/org/mobicents/servlet/sip/testsuite/package-info.java
+++
@@ -1,3 +0,0 @@
-@javax.servlet.sip.annotation.SipApplication(name="org.mobicents.servlet.sip.testsuite.AnnotatedApplication")
-@org.mobicents.servlet.sip.annotation.ConcurrencyControl(mode=org.mobicents.servlet.sip.annotation.ConcurrencyControlMode.SipApplicationSession)
-package org.mobicents.servlet.sip.testsuite;
