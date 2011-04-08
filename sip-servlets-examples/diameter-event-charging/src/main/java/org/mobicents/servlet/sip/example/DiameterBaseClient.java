08a2f691936c76c814cdf40f990c72ed6e75ba1d
jean.deruelle@gmail.com <jean.deruelle@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2011-04-08 12:29:58 +0000

    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@17821 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ sip-servlets-examples/diameter-event-charging/src/main/java/org/mobicents/servlet/sip/example/DiameterBaseClient.java
@@ -0,0 +1,272 @@
+package org.mobicents.servlet.sip.example;
+
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanException;
+import javax.management.MBeanServer;
+import javax.management.MBeanServerFactory;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import javax.management.ReflectionException;
+
+import org.apache.log4j.Logger;
+import org.jdiameter.api.Answer;
+import org.jdiameter.api.ApplicationId;
+import org.jdiameter.api.Avp;
+import org.jdiameter.api.AvpDataException;
+import org.jdiameter.api.AvpSet;
+import org.jdiameter.api.Message;
+import org.jdiameter.api.Request;
+import org.mobicents.diameter.api.DiameterMessageFactory;
+import org.mobicents.diameter.api.DiameterProvider;
+import org.mobicents.diameter.dictionary.AvpDictionary;
+import org.mobicents.diameter.stack.DiameterListener;
+import org.mobicents.diameter.stack.DiameterStackMultiplexer;
+
+public class DiameterBaseClient implements DiameterProvider, DiameterListener
+{
+
+  private static final long serialVersionUID = 1L;
+ 
+  private static Logger logger = Logger.getLogger(DiameterBaseClient.class);
+
+  private DiameterStackMultiplexer muxMBean = null;
+  private DiameterMessageFactory msgFactory = null;
+  private DiameterProvider provider = null;
+  
+  public DiameterBaseClient() throws InstanceNotFoundException, MBeanException, ReflectionException,  NullPointerException, MalformedObjectNameException
+  {
+    ObjectName objectName = new ObjectName("diameter.mobicents:service=DiameterStackMultiplexer");
+    
+    DiameterListener listener = this;
+
+    ApplicationId[] appIds = new ApplicationId[]{ApplicationId.createByAccAppId( 193, 19302L )};
+    
+    Object[] params = new Object[]{};
+
+    String[] signature = new String[]{};
+    
+    String operation = "getMultiplexerMBean";
+    
+    MBeanServer server = (MBeanServer) MBeanServerFactory.findMBeanServer(null).get(0);
+    
+    Object object = server.invoke( objectName, operation, params, signature );
+    
+    if(object instanceof DiameterStackMultiplexer)
+      muxMBean = (DiameterStackMultiplexer) object;
+    
+    logger.info( "muxMBean == " + muxMBean );
+    
+    muxMBean.registerListener( listener, appIds );
+    
+    msgFactory =  muxMBean.getMessageFactory();
+
+    provider = muxMBean.getProvider();
+    
+  }
+  
+  public String sendMessage( Message message )
+  {
+    return this.provider.sendMessage( message );
+  }
+
+  public Answer processRequest( Request request )
+  {
+    // We don't deal with this...
+    return null;
+  }
+
+  public void receivedSuccessMessage( Request request, Answer answer )
+  {
+    logger.info( "Received success message (Result-Code[" + answer.getResultCode() + "]) for Diameter Request with Session-Id [" + request.getSessionId() + "]" );
+  }
+
+  public void timeoutExpired( Request request )
+  {
+    logger.info( "Timeout expired for Diameter Request with Session-Id [" + request.getSessionId() + "]" );
+  }
+
+  public Message createAccountingRequest(String sessionId, String uid, Long value, boolean refund)
+  {
+    //<command name="ACR">
+    Request req = (Request) this.msgFactory.createRequest( 271, 19302L );
+    
+    req.setProxiable( true );
+    
+    AvpSet avps = req.getAvps();
+
+    //<avp name="Origin-Host" code="264" vendor="0" value="localhost" />
+    avps.addAvp( Avp.ORIGIN_HOST, "aaa://127.0.0.1:1812".getBytes(), true, false );
+    //<avp name="Origin-Realm" code="296" vendor="0" value="mobicents.org" />
+    avps.addAvp( Avp.ORIGIN_REALM, "mobicents.org".getBytes(), true, false );
+    
+    //<avp name="Session-Id" code="263" vendor="0" value="12345" />
+    avps.addAvp( Avp.SESSION_ID, sessionId.getBytes(), true, false );
+    
+    //<avp name="Vendor-Specific-Application-Id" code="260" vendor="0">
+    //  <avp name="Vendor-Id" code="266" vendor="0" value="193" />
+    //  <avp name="Acct-Application-Id" code="259" vendor="0" value="19302" />
+    //</avp>
+    AvpSet vsai = avps.addGroupedAvp( Avp.VENDOR_SPECIFIC_APPLICATION_ID, true, false );
+    vsai.addAvp( 266, 193, true, false, true );
+    vsai.addAvp( 259, 19302, true, false, true );
+    
+    //<avp name="Destination-Realm" code="283" vendor="0" value="mobicents.org" />
+    avps.addAvp( Avp.DESTINATION_REALM, "mobicents.org".getBytes(), true, false );
+    
+    //<avp name="Destination-Host" code="293" vendor="0" value="localhost" />
+    avps.addAvp( Avp.DESTINATION_HOST, "aaa://127.0.0.1:21812".getBytes(), true, false );
+    
+    //<avp name="SCAP-Subscription-Id" code="553" vendor="193">
+    //  <avp name="SCAP-Subscription-Id-Type" code="555" vendor="193" value="0" />
+    //  <avp name="SCAP-Subscription-Id-Data" code="554" vendor="193" value="00001000" />
+    //</avp>
+    AvpSet ssi = avps.addGroupedAvp( 553, 193, true, false );
+    ssi.addAvp( 555, 0, 193, true, false );
+    ssi.addAvp( 554, uid.getBytes(), 193, true, false );
+
+    //<avp name="SCAP-Requested-Service-Unit" code="606" vendor="193">
+    //  <avp name="SCAP-Unit-Type" code="611" vendor="193" value="2" />
+    //  <avp name="SCAP-Unit-Value" code="612" vendor="193">
+    //    <avp name="SCAP-Value-Digits" code="617" vendor="193" value="10" />
+    //  </avp>
+    //  <avp name="SCAP-Currency-Code" code="544" vendor="193" value="978" />
+    //</avp>
+    AvpSet srsu = avps.addGroupedAvp( 606, 193, true, false );
+    srsu.addAvp( 611, 3, 193, true, false );
+    AvpSet suv = srsu.addGroupedAvp( 612, 193, true, false );
+    suv.addAvp( 617, value, 193, true, false );
+    srsu.addAvp( 544, 978, 193, true, false );
+
+    //<avp name="Accounting-Record-Number" code="485" vendor="0" value="0" />
+    avps.addAvp( Avp.ACC_RECORD_NUMBER, 0, true, false );
+    
+    //<avp name="Accounting-Record-Type" code="480" vendor="0" value="1" />
+    avps.addAvp( Avp.ACC_RECORD_TYPE, 1, true, false );
+    
+    //<avp name="SCAP-Requested-Action" code="615" vendor="193" value="0" />
+    avps.addAvp( 615, refund ? 1 : 0, 193, true, false );
+    
+    //<avp name="SCAP-Service-Parameter-Info" code="607" vendor="193">
+    //  <avp name="SCAP-Service-Parameter-Type" code="608" vendor="193" value="0" />
+    //  <avp name="SCAP-Service-Parameter-Value" code="609" vendor="193" value="510" />
+    //</avp>
+    AvpSet sspi1 = avps.addGroupedAvp( 607, 193, true, false );
+    sspi1.addAvp( 608, 0, 193, true, false );
+    sspi1.addAvp( 609, "510".getBytes(), 193, true, false );
+
+    //<avp name="SCAP-Service-Parameter-Info" code="607" vendor="193">
+    //  <avp name="SCAP-Service-Parameter-Type" code="608" vendor="193" value="14" />
+    //  <avp name="SCAP-Service-Parameter-Value" code="609" vendor="193" value="20" />
+    //</avp>
+    AvpSet sspi2 = avps.addGroupedAvp( 607, 193, true, false );
+    sspi2.addAvp( 608, 14, 193, true, false );
+    sspi2.addAvp( 609, "20".getBytes(), 193, true, false );
+    
+    return req;
+  }
+
+  public Message sendMessageSync( Message message )
+  {
+    logger.info( printMessage( message ) );
+    return this.provider.sendMessageSync( message );
+  }
+  
+  private String printMessage( Message message )
+  {
+    String toString = "\r\n" +
+        "+----------------------------------- HEADER ----------------------------------+\r\n" +
+        "| Version................." + message.getVersion() + "\r\n" +
+        "| Command-Flags..........." + "R[" + message.isRequest() + "] P[" + message.isProxiable() + "] " +
+           "E[" + message.isError() + "] T[" + message.isReTransmitted() + "]" + "\r\n" +
+        "| Command-Code............" + message.getCommandCode() + "\r\n" +
+        "| Application-Id.........." + message.getApplicationId() + "\r\n" +
+        "| Hop-By-Hop Identifier..." + message.getHopByHopIdentifier() + "\r\n" +
+        "| End-To-End Identifier..." + message.getEndToEndIdentifier() + "\r\n" +
+        "+------------------------------------ AVPs -----------------------------------+\r\n";
+
+    for( Avp avp : message.getAvps() )
+    {
+      toString += printAvp( avp, "" );
+    }
+    
+    toString += "+-----------------------------------------------------------------------------+\r\n";
+    
+    return toString;    
+  }
+  
+  private String printAvp(Avp avp, String indent)
+  {
+    Object avpValue = null;
+    String avpString = "";
+    boolean isGrouped = false;
+    
+    try
+    {
+      String avpType = AvpDictionary.INSTANCE.getAvp( avp.getCode(), avp.getVendorId() ).getType();
+      
+      if("Integer32".equals(avpType) || "AppId".equals(avpType))
+      {
+        avpValue = avp.getInteger32();
+      }
+      else if("Unsigned32".equals(avpType) || "VendorId".equals(avpType))
+      {
+        avpValue = avp.getUnsigned32();
+      }
+      else if("Float64".equals(avpType))
+      {
+        avpValue = avp.getFloat64();
+      }
+      else if("Integer64".equals(avpType))
+      {
+        avpValue = avp.getInteger64();
+      }
+      else if("Time".equals(avpType))
+      {
+        avpValue = avp.getTime();
+      }
+      else if("Unsigned64".equals(avpType))
+      {
+        avpValue = avp.getUnsigned64();
+      }
+      else if("Grouped".equals(avpType))
+      {
+        avpValue = "<Grouped>";
+        isGrouped = true;
+      }
+      else
+      {
+        avpValue = avp.getOctetString().replaceAll( "\r", "" ).replaceAll( "\n", "" );
+      }
+    }
+    catch (Exception ignore) {
+      try
+      {
+        avpValue = avp.getOctetString().replaceAll( "\r", "" ).replaceAll( "\n", "" );
+      }
+      catch ( AvpDataException e ) {
+        avpValue = avp.toString();
+      }
+    }
+    
+    avpString += "| " + indent + "AVP: Code[" + avp.getCode() + "] VendorID[" + avp.getVendorId() + "] Value[" + 
+    avpValue + "] Flags[M=" + avp.isMandatory() + ";E=" + avp.isEncrypted() + ";V=" + avp.isVendorId() + "]\r\n";
+    
+    if(isGrouped)
+    {
+      try
+      {
+        for(Avp subAvp : avp.getGrouped())
+        {
+          avpString += printAvp( subAvp, indent + "  " );          
+        }
+      }
+      catch ( AvpDataException e )
+      {
+        // Failed to ungroup... ignore then...
+      }
+    }
+
+    return avpString;
+  }
+  
+}
---
+++ sip-servlets-examples/diameter-event-charging/src/main/java/org/mobicents/servlet/sip/example/DiameterEventChargingSipServlet.java
@@ -0,0 +1,262 @@
+/*
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.mobicents.servlet.sip.example;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.sip.Address;
+import javax.servlet.sip.Proxy;
+import javax.servlet.sip.SipApplicationSession;
+import javax.servlet.sip.SipFactory;
+import javax.servlet.sip.SipServlet;
+import javax.servlet.sip.SipServletRequest;
+import javax.servlet.sip.SipServletResponse;
+import javax.servlet.sip.SipSession;
+import javax.servlet.sip.URI;
+
+import org.apache.log4j.Logger;
+import org.jdiameter.api.Answer;
+import org.jdiameter.api.Message;
+import org.jdiameter.api.Request;
+
+/**
+ * This example shows how to do Diameter Event Charging.
+ * It is based on the location service example. 
+ * @author Alexandre Mendonca
+ *
+ */
+public class DiameterEventChargingSipServlet extends SipServlet {
+
+  private static final long serialVersionUID = 1L;
+  
+  private static Logger logger = Logger.getLogger(DiameterEventChargingSipServlet.class);
+  private static final String CONTACT_HEADER = "Contact";
+  Map<String, List<URI>> registeredUsers = null;
+  
+  DiameterBaseClient diameterBaseClient = null;
+  
+  private static final String CHARGING_MSDN_ID = "00001000";
+  private static final long CHARGING_VALUE = 10L; 
+  
+  /** Creates a new instance of SpeedDialSipServlet */
+  public DiameterEventChargingSipServlet() {}
+
+  @Override
+  public void init(ServletConfig servletConfig) throws ServletException {
+    logger.info("the locationb service sip servlet has been started");
+    super.init(servletConfig);
+    SipFactory sipFactory = (SipFactory)getServletContext().getAttribute(SIP_FACTORY);
+    registeredUsers = new HashMap<String, List<URI>>();
+    List<URI> uriList  = new ArrayList<URI>();
+    uriList.add(sipFactory.createURI("sip:receiver@127.0.0.1:5090"));
+    //uriList.add(sipFactory.createURI("sip:receiver@127.0.0.1:6090"));
+    registeredUsers.put("sip:receiver@sip-servlets.com", uriList);
+    List<URI> perfUriList  = new ArrayList<URI>();
+    perfUriList.add(sipFactory.createURI("sip:perf-receiver@127.0.0.1:5090"));    
+    registeredUsers.put("sip:perf-receiver@sip-servlets.com", perfUriList);
+    ArrayList<URI> failOverUriList  = new ArrayList<URI>();
+    failOverUriList.add(sipFactory.createURI("sip:receiver-failover@127.0.0.1:5090"));
+    registeredUsers.put("sip:receiver-failover@sip-servlets.com", failOverUriList);
+    
+    // Initialize diameter client
+    try {
+      diameterBaseClient = new DiameterBaseClient();
+    }
+    catch ( Exception e ) {
+      logger.error( "Failure initializing Diameter Base Client.", e );
+    }   
+  }
+
+  @Override
+  protected void doInvite(SipServletRequest request) throws ServletException,
+      IOException {
+
+    logger.info("Got request:\n" + request.toString());   
+    
+    List<URI> contactAddresses = registeredUsers.get(request.getRequestURI().toString());
+    if(contactAddresses != null && contactAddresses.size() > 0) {
+      
+      // We'll do the charging here (false means it's a debit, not a refund).
+      long resultCode = doDiameterCharging( request.getCallId(), CHARGING_MSDN_ID, CHARGING_VALUE, false );
+      
+      /*
+       * Check the response:
+       * 
+      -  1xxx (Informational)
+      -  2xxx (Success)
+      -  3xxx (Protocol Errors)
+      -  4xxx (Transient Failures)
+      -  5xxx (Permanent Failure)
+      */
+      if(resultCode >= 1000 & resultCode < 2000 )
+      {
+        logger.info("Diameter Answer == Informational (" + resultCode + ") ... Unexpected! Aborting!");
+        SipServletResponse sipServletResponse = request.createResponse( SipServletResponse.SC_SERVICE_UNAVAILABLE );
+        sipServletResponse.send();
+        return;
+      }
+      else if(resultCode >= 2000 & resultCode < 3000 )
+      {
+        logger.info("Diameter Answer == Success ... Proceeding!");
+      }
+      else if(resultCode >= 3000 & resultCode < 4000 )
+      {
+        logger.info("Diameter Answer == Protocol Error (" + resultCode + ") ... Ooops!");
+        SipServletResponse sipServletResponse = request.createResponse( SipServletResponse.SC_SERVICE_UNAVAILABLE );
+        sipServletResponse.send();
+        return;
+      }
+      else if(resultCode >= 4000 & resultCode < 5000 )
+      {
+        logger.info("Diameter Answer == Transient Failure (" + resultCode + ") ... Aborting! Let the user try again...");
+
+        SipServletResponse sipServletResponse;
+        
+        if(resultCode == 4241)
+        {
+          sipServletResponse = request.createResponse( SipServletResponse.SC_PAYMENT_REQUIRED );
+        }
+        else
+        {
+          sipServletResponse = request.createResponse( SipServletResponse.SC_SERVICE_UNAVAILABLE );
+        }
+
+        sipServletResponse.send();
+        return;
+      }
+      else if(resultCode >= 5000 )
+      {
+        logger.info( "Diameter Answer == Permanent Failure (" + resultCode + ") ... Aborting!" );
+        SipServletResponse sipServletResponse = request.createResponse( SipServletResponse.SC_SERVICE_UNAVAILABLE );
+        sipServletResponse.send();
+        return;
+      }
+
+      Proxy proxy = request.getProxy();
+      proxy.setRecordRoute(true);
+      proxy.setParallel(true);
+      proxy.setSupervised(true);
+      proxy.proxyTo(contactAddresses);    
+    } else {
+      logger.info(request.getRequestURI().toString() + " is not currently registered");
+      SipServletResponse sipServletResponse = 
+        request.createResponse(SipServletResponse.SC_MOVED_PERMANENTLY, "Moved Permanently");
+      sipServletResponse.send();
+    }
+  }
+
+  @Override
+  protected void doRegister(SipServletRequest req) throws ServletException,
+      IOException {
+    logger.info("Received register request: " + req.getTo());
+    int response = SipServletResponse.SC_OK;
+    SipServletResponse resp = req.createResponse(response);
+    HashMap<String, String> users = (HashMap<String, String>) getServletContext().getAttribute("registeredUsersMap");
+    if(users == null) users = new HashMap<String, String>();
+    getServletContext().setAttribute("registeredUsersMap", users);
+    
+    Address address = req.getAddressHeader(CONTACT_HEADER);
+    String fromURI = req.getFrom().getURI().toString();
+    
+    int expires = address.getExpires();
+    if(expires < 0) {
+      expires = req.getExpires();
+    }
+    if(expires == 0) {
+      users.remove(fromURI);
+      logger.info("User " + fromURI + " unregistered");
+    } else {
+      resp.setAddressHeader(CONTACT_HEADER, address);
+      users.put(fromURI, address.getURI().toString());
+      logger.info("User " + fromURI + 
+          " registered with an Expire time of " + expires);
+    }       
+            
+    resp.send();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  protected void doResponse(SipServletResponse response)
+      throws ServletException, IOException {
+
+    logger.info("SimpleProxyServlet: Got response:\n" + response);
+    if(SipServletResponse.SC_OK == response.getStatus() && "BYE".equalsIgnoreCase(response.getMethod())) {
+      SipSession sipSession = response.getSession(false);
+      if(sipSession != null) {
+        SipApplicationSession sipApplicationSession = sipSession.getApplicationSession();
+        sipSession.invalidate();
+        sipApplicationSession.invalidate();
+      }     
+    }
+    else if ( response.getStatus() >= 400) {
+      doErrorResponse(response);
+    }
+  }
+  
+  @Override
+  protected void doCancel( SipServletRequest req ) throws ServletException, IOException
+  {
+    doDiameterCharging( req.getCallId(), CHARGING_MSDN_ID, CHARGING_VALUE, true );
+  }
+  
+  @Override
+  protected void doErrorResponse( SipServletResponse resp ) throws ServletException, IOException
+  {
+    doDiameterCharging( resp.getCallId(), CHARGING_MSDN_ID, CHARGING_VALUE, true );
+  }
+  
+  /**
+   * Method for doing the Charging, either it's a debit or a refund.
+   * 
+   * @param sessionId the Session-Id for the Diameter Message
+   * @param userId the User-Id of the client in the Diameter Server
+   * @param cost the Cost (or Refund value) of the service 
+   * @param refund boolean indicating if it's a refund or not
+   * @return a long with the Result-Code AVP from the Answer
+   */
+  private long doDiameterCharging(String sessionId, String userId, Long cost, boolean refund)
+  {
+    try
+    {
+      logger.info( "Creating Diameter Charging " + (refund ? "Refund" : "Debit") + " Request UserId[" + userId + "], Cost[" + cost + "]..." );
+      Request req = (Request) diameterBaseClient.createAccountingRequest( sessionId, userId, cost, refund );
+      
+      logger.info( "Sending Diameter Charging " + (refund ? "Refund" : "Debit") + " Request..." );
+      Message msg = diameterBaseClient.sendMessageSync( req );
+
+      long resultCode = -1;
+      
+      if(msg instanceof Answer)
+        resultCode = ((Answer)msg).getResultCode().getUnsigned32();
+      
+      return resultCode;
+    }
+    catch ( Exception e )
+    {
+      logger.error( "Failure communicating with Diameter Charging Server.", e );
+      return 5012;
+    }   
+  }
+}
