efe52291e3de0619fd093083d1bdf1e5e9c3da87
jean.deruelle@gmail.com <jean.deruelle@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2011-05-20 12:35:33 +0000

    Update Issue 2580
    Fixing regression on CANCEL test cases
    
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@18541 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-impl/src/main/java/org/mobicents/servlet/sip/message/SipServletRequestImpl.java
+++ sip-servlets-impl/src/main/java/org/mobicents/servlet/sip/message/SipServletRequestImpl.java
@@ -333,9 +333,9 @@ public class SipServletRequestImpl extends SipServletMessageImpl implements
 					// if a dialog has already been created
 					// reuse local tag
 					final Dialog dialog = transaction.getDialog();
-					if(dialog != null && dialog.getLocalTag() != null && dialog.getLocalTag().length() > 0
+					if(session != null && dialog != null && dialog.getLocalTag() != null && dialog.getLocalTag().length() > 0
 							&& session.getKey().getToTag() != null && session.getKey().getToTag().length() >0) {
-						if((session != null && !dialog.getLocalTag().equals(session.getKey().getToTag()))) {
+						if(!dialog.getLocalTag().equals(session.getKey().getToTag())) {
 							// Issue 2354 : if the dialog to tag is different than the  session to tag use the session to tag
 							// so that we send the forked response out with the correct to tag
 							if(logger.isDebugEnabled()) {
