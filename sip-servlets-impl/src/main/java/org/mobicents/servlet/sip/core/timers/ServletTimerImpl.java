73a6392176e3e13d27b1d95a924e9c022d98e808
jean.deruelle@gmail.com <jean.deruelle@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2011-07-06 13:19:53 +0000

    Update Issue 2637
    nullifying the ref to the serilizable info as well to avoid it lingering around until JDK decides to collect the timer
    
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-servlets@19292 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets-impl/src/main/java/org/mobicents/servlet/sip/core/timers/ServletTimerImpl.java
+++ sip-servlets-impl/src/main/java/org/mobicents/servlet/sip/core/timers/ServletTimerImpl.java
@@ -190,6 +190,8 @@ public class ServletTimerImpl implements MobicentsServletTimer, Runnable {
 				// be due for gc anyway....
 				appSessionToCancelThisTimersFrom = getApplicationSession();
 				future = null;
+				// Issue 2367 removing the reference to the Serializable info so that it doesn't linger into memory
+				info = null;
 			}
 		}
 		if (appSessionToCancelThisTimersFrom != null && updateAppSessionReadyToInvalidateState) {
