944110bcb42b1599db649f8824e8596a6a81dcef
Jean Deruelle <jean.deruelle@gmail.com>
2012-05-21 22:17:52 +0200

    Update Issue 90
    Backporting revision 21071 from old JBCP mobicents repo

--- containers/sip-servlets-jboss5/src/main/java/org/mobicents/servlet/sip/core/timers/TimerServiceTaskData.java
+++ containers/sip-servlets-jboss5/src/main/java/org/mobicents/servlet/sip/core/timers/TimerServiceTaskData.java
@@ -22,46 +22,43 @@
 
 package org.mobicents.servlet.sip.core.timers;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.ObjectStreamClass;
 import java.io.Serializable;
 
+import org.apache.log4j.Logger;
 import org.mobicents.servlet.sip.core.session.MobicentsSipApplicationSessionKey;
-import org.mobicents.servlet.sip.core.session.SipApplicationSessionKey;
 import org.mobicents.timers.PeriodicScheduleStrategy;
 import org.mobicents.timers.TimerTaskData;
 
 /**
  * @author jean.deruelle@gmail.com
- *
+ * 
  */
 public class TimerServiceTaskData extends TimerTaskData implements Serializable {
-	
-	private Serializable data;
-	
+
+	private static final Logger logger = Logger.getLogger(TimerServiceTaskData.class);
+
+	private transient Serializable data;
+	private transient byte[] dataBytes;
+
 	private MobicentsSipApplicationSessionKey key;
-	
+
 	private long delay;
-	
+
 	public TimerServiceTaskData(Serializable id, long startTime, long period,
 			PeriodicScheduleStrategy periodicScheduleStrategy) {
 		super(id, startTime, period, periodicScheduleStrategy);
 	}
 
 	/**
-	 * @param data the data to set
-	 */
-	public void setData(Serializable data) {
-		this.data = data;
-	}
-
-	/**
-	 * @return the data
-	 */
-	public Serializable getData() {
-		return data;
-	}
-
-	/**
-	 * @param key the key to set
+	 * @param key
+	 *            the key to set
 	 */
 	public void setKey(MobicentsSipApplicationSessionKey key) {
 		this.key = key;
@@ -75,7 +72,8 @@ public class TimerServiceTaskData extends TimerTaskData implements Serializable 
 	}
 
 	/**
-	 * @param delay the delay to set
+	 * @param delay
+	 *            the delay to set
 	 */
 	public void setDelay(long delay) {
 		this.delay = delay;
@@ -88,4 +86,147 @@ public class TimerServiceTaskData extends TimerTaskData implements Serializable 
 		return delay;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
+	public Serializable getData() {
+		if(data != null) {
+			return data;
+		}
+		if (dataBytes != null) {			
+			deserializeData();
+		}
+		return data;
+	}
+	
+	/**
+	 * 
+	 * @param data
+	 */
+	public void setData(Serializable data) {
+		this.data = data;
+		this.dataBytes = null;
+	}
+	
+	// http://code.google.com/p/sipservlets/issues/detail?id=90
+	
+	private void serializeData() {
+		ObjectOutputStream out = null;
+		ByteArrayOutputStream bos = null;
+		try {
+			bos = new ByteArrayOutputStream() ;
+			out = new ObjectOutputStream(bos) ;
+			out.writeObject(data);
+			out.close();
+			dataBytes = bos.toByteArray();
+			out = null;
+			bos = null;
+		}
+		catch (Exception e) {
+			logger.error("failed to serialize timer app data "+data+" , it won't be available in another cluster node",e);
+		}
+		finally {
+			if (out != null) {
+				try {
+					out.close();
+				}
+				catch (Exception e) {
+					if(logger.isDebugEnabled()) {
+						logger.debug(e.getMessage(),e);
+					}
+				}
+			}
+			else {
+				if (bos != null) {
+					try {
+						bos.close();
+					}
+					catch (Exception e) {
+						if(logger.isDebugEnabled()) {
+							logger.debug(e.getMessage(),e);
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	private void deserializeData() {
+		ObjectInputStream in = null;
+		ByteArrayInputStream bis = null;
+		try {
+			bis = new ByteArrayInputStream(dataBytes) ;
+			in = new CustomObjectInputStream(bis) ;
+			data = (Serializable) in.readObject();
+			in.close();
+			in = null;
+			bis = null;
+		}
+		catch (Exception e) {
+			logger.error("failed to deserialize timer app data: "+e.getMessage(),e);
+		}
+		finally {
+			if (in != null) {
+				try {
+					in.close();
+				}
+				catch (Exception e) {
+					if(logger.isDebugEnabled()) {
+						logger.debug(e.getMessage(),e);
+					}					
+				}
+			}
+			else {
+				if (bis != null) {
+					try {
+						bis.close();
+					}
+					catch (Exception e) {
+						if(logger.isDebugEnabled()) {
+							logger.debug(e.getMessage(),e);
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	private static class CustomObjectInputStream extends ObjectInputStream {
+
+		public CustomObjectInputStream(InputStream in) throws IOException {
+			super(in);
+		}
+		
+		@Override
+		protected Class<?> resolveClass(ObjectStreamClass desc)
+				throws IOException, ClassNotFoundException {
+			return Thread.currentThread().getContextClassLoader().loadClass(desc.getName());			
+		}
+	}
+	
+	private void readObject(java.io.ObjectInputStream in)
+			throws IOException, ClassNotFoundException {
+		in.defaultReadObject();
+		int length = in.readInt();
+		if (length > 0) {
+			dataBytes = new byte[length];
+			in.readFully(dataBytes);
+		}
+	}
+
+	private void writeObject(java.io.ObjectOutputStream out)
+			throws IOException {
+		out.defaultWriteObject();
+		if (dataBytes == null && data != null) {
+			serializeData();
+		}
+		if(dataBytes == null) {
+			out.writeInt(0);
+		}
+		else {
+			out.writeInt(dataBytes.length);
+			out.write(dataBytes);
+		}		
+	}
 }
