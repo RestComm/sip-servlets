8715c3e6f30c5889348478fbee213a57207295c1
Jean Deruelle <jean.deruelle@gmail.com>
2012-08-10 18:37:28 +0200

    fixing license headers

--- containers/sip-servlets-as7-drop-in/jboss-as-mobicents/src/main/java/org/mobicents/metadata/sip/parser/SipCommonMetaDataParser.java
+++ containers/sip-servlets-as7-drop-in/jboss-as-mobicents/src/main/java/org/mobicents/metadata/sip/parser/SipCommonMetaDataParser.java
@@ -1,8 +1,8 @@
 /*
- * JBoss, Home of Professional Open Source.
- * Copyright 2010, Red Hat, Inc., and individual contributors
- * as indicated by the @author tags. See the copyright.txt file in the
- * distribution for a full listing of individual contributors.
+ * TeleStax, Open Source Cloud Communications  Copyright 2012. 
+ * and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
@@ -19,7 +19,6 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-
 package org.mobicents.metadata.sip.parser;
 
 import java.util.ArrayList;
