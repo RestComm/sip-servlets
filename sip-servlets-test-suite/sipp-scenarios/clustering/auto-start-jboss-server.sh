475b8146075aec019866c61d2651026577251d12
Jean Deruelle <jean.deruelle@gmail.com>
2012-08-29 15:45:40 +0200

    Backport to master
    trying to fix the script according to http://stackoverflow.com/questions/818255/in-the-bash-shell-what-is-21
    (cherry picked from commit 975d09cf0cab40512c8746443c00ecd7b53cb633)

--- sip-servlets-test-suite/sipp-scenarios/clustering/auto-start-jboss-server.sh
+++ sip-servlets-test-suite/sipp-scenarios/clustering/auto-start-jboss-server.sh
@@ -1,9 +1,9 @@
 export JAVA_OPTS="-server -Xms1536m -Xmx1536m -XX:PermSize=128M -XX:MaxPermSize=256M -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode"
 if [ "0" = $3 ]; then
   echo "Port configuration is 0. Using default command. No need for hacks."
-  $JBOSS_HOME/bin/run.sh -Djboss.server.log.threshold=INFO -c $1 &> "jboss-$1-$4.out"&
+  $JBOSS_HOME/bin/run.sh -Djboss.server.log.threshold=INFO -c $1 > jboss-$1-$4.out 2>&1 &
 else
-  $JBOSS_HOME/bin/run.sh -Djboss.server.log.threshold=INFO -c $1 -Djboss.service.binding.set=ports-0$3 -Djboss.messaging.ServerPeerID=$3 &> "jboss-$1-$4.out"&
+  $JBOSS_HOME/bin/run.sh -Djboss.server.log.threshold=INFO -c $1 -Djboss.service.binding.set=ports-0$3 -Djboss.messaging.ServerPeerID=$3 > jboss-$1-$4.out 2>&1 &
 fi
 export jbosspid=$!
 echo "Starting app server. The pid for $2 is $jbosspid and other options ports are $3 config is $4"
