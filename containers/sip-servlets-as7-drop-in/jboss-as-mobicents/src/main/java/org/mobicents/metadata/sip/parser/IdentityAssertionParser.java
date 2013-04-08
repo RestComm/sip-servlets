4718b6a5364bf79925c5f58b227715058bd53213
josemrecio <josemrecio@gmail.com>
2013-04-08 12:05:58 +0200

    Update Issue 204
    Backport to master
    
    Added support for identity-assertion configuration via sip.xml
    (cherry picked from commit 3afcc4466352fb2e0df8e2de7d8383e517a7ab09)

---
+++ containers/sip-servlets-as7-drop-in/jboss-as-mobicents/src/main/java/org/mobicents/metadata/sip/parser/IdentityAssertionParser.java
@@ -0,0 +1,76 @@
+/*
+ * TeleStax, Open Source Cloud Communications  Copyright 2012. 
+ * and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.mobicents.metadata.sip.parser;
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+import org.jboss.metadata.parser.util.MetaDataElementParser;
+import org.mobicents.metadata.sip.spec.Attribute;
+import org.mobicents.metadata.sip.spec.Element;
+import org.mobicents.metadata.sip.spec.IdentityAssertionMetaData;
+
+/**
+ * @author josemrecio@gmail.com
+ */
+public class IdentityAssertionParser extends MetaDataElementParser {
+
+    public static IdentityAssertionMetaData parse(XMLStreamReader reader) throws XMLStreamException {
+        IdentityAssertionMetaData idAssertionConfig = new IdentityAssertionMetaData();
+
+        // Handle attributes
+        final int count = reader.getAttributeCount();
+        for (int i = 0; i < count; i++) {
+            final String value = reader.getAttributeValue(i);
+            if (attributeHasNamespace(reader, i)) {
+                continue;
+            }
+            final Attribute attribute = Attribute.forName(reader.getAttributeLocalName(i));
+            switch (attribute) {
+                case ID: {
+                    idAssertionConfig.setId(value);
+                    break;
+                }
+                default:
+                    throw unexpectedAttribute(reader, i);
+            }
+        }
+
+        // Handle elements
+        while (reader.hasNext() && reader.nextTag() != END_ELEMENT) {
+            final Element element = Element.forName(reader.getLocalName());
+            switch (element) {
+                case IDENTITY_ASSERTION_SCHEME:
+                    idAssertionConfig.setIdentityAssertionScheme(getElementText(reader));
+                    break;
+                case IDENTITY_ASSERTION_SUPPORT:
+                    idAssertionConfig.setIdentityAssertionSupport(getElementText(reader));
+                    break;
+                default:
+                    throw unexpectedElement(reader);
+            }
+        }
+
+        return idAssertionConfig;
+    }
+
+}
