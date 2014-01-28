7f0683b243c20bf81d72557e408a997f5de0d0a3
Jean Deruelle <jean.deruelle@telestax.com>
2014-01-28 12:34:15 +0100

    Update Issue 203
    Fix bad merge

--- containers/sip-servlets-as7-drop-in/jboss-as-mobicents/src/main/java/org/mobicents/metadata/sip/parser/IdentityAssertionParser.java
+++ containers/sip-servlets-as7-drop-in/jboss-as-mobicents/src/main/java/org/mobicents/metadata/sip/parser/IdentityAssertionParser.java
@@ -1,23 +1,20 @@
 /*
- * TeleStax, Open Source Cloud Communications  Copyright 2012. 
- * and individual contributors
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
+ * TeleStax, Open Source Cloud Communications
+ * Copyright 2011-2013, Telestax Inc and individual contributors
+ * by the @authors tag.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
+ * This program is free software: you can redistribute it and/or modify
+ * under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3 of
  * the License, or (at your option) any later version.
  *
- * This software is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 package org.mobicents.metadata.sip.parser;
 
@@ -35,11 +32,7 @@ import org.mobicents.metadata.sip.spec.IdentityAssertionMetaData;
 public class IdentityAssertionParser extends MetaDataElementParser {
 
     public static IdentityAssertionMetaData parse(XMLStreamReader reader) throws XMLStreamException {
-<<<<<<< HEAD
         IdentityAssertionMetaData idAssertionConfig = new IdentityAssertionMetaData();
-=======
-    	IdentityAssertionMetaData idAssertionConfig = new IdentityAssertionMetaData();
->>>>>>> 567e843... Initial commit for Issue 203: 	Upgrade to jboss-as-7.2.0.Final
 
         // Handle attributes
         final int count = reader.getAttributeCount();
@@ -51,11 +44,7 @@ public class IdentityAssertionParser extends MetaDataElementParser {
             final Attribute attribute = Attribute.forName(reader.getAttributeLocalName(i));
             switch (attribute) {
                 case ID: {
-<<<<<<< HEAD
                     idAssertionConfig.setId(value);
-=======
-                	idAssertionConfig.setId(value);
->>>>>>> 567e843... Initial commit for Issue 203: 	Upgrade to jboss-as-7.2.0.Final
                     break;
                 }
                 default:
@@ -68,19 +57,11 @@ public class IdentityAssertionParser extends MetaDataElementParser {
             final Element element = Element.forName(reader.getLocalName());
             switch (element) {
                 case IDENTITY_ASSERTION_SCHEME:
-<<<<<<< HEAD
                     idAssertionConfig.setIdentityAssertionScheme(getElementText(reader));
                     break;
                 case IDENTITY_ASSERTION_SUPPORT:
                     idAssertionConfig.setIdentityAssertionSupport(getElementText(reader));
                     break;
-=======
-                	idAssertionConfig.setIdentityAssertionScheme(getElementText(reader));
-                    break;
-                case IDENTITY_ASSERTION_SUPPORT:
-                	idAssertionConfig.setIdentityAssertionSupport(getElementText(reader));
-                	break;
->>>>>>> 567e843... Initial commit for Issue 203: 	Upgrade to jboss-as-7.2.0.Final
                 default:
                     throw unexpectedElement(reader);
             }
