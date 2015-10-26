f7cae371aefbc4fd453e2c3ce03c2024fbdaceb2
alerant.appngin <alerant.appngin@gmail.com>
2015-10-26 13:12:38 +0100

    Adding purge() calls to proxy timer service to fix Issue #80
    Adding purge() calls to proxy timer service to fix Issue #80

--- sip-servlets-impl/src/main/java/org/mobicents/servlet/sip/core/timers/ProxyTimerServiceImpl.java
+++ sip-servlets-impl/src/main/java/org/mobicents/servlet/sip/core/timers/ProxyTimerServiceImpl.java
@@ -27,55 +27,102 @@ import java.util.TimerTask;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.apache.log4j.Logger;
+import org.mobicents.servlet.sip.startup.StaticServiceHolder;
 
 /**
  * @author jean.deruelle@gmail.com
+ * @author kakonyi.istvan@alerant.hu
  *
  */
 public class ProxyTimerServiceImpl extends Timer implements ProxyTimerService {
 
-	private static final Logger logger = Logger.getLogger(ProxyTimerServiceImpl.class
-			.getName());
-	private AtomicBoolean started = new AtomicBoolean(false);
+    private static final Logger logger = Logger.getLogger(ProxyTimerServiceImpl.class
+            .getName());
+    private AtomicBoolean started = new AtomicBoolean(false);
 
-	/* (non-Javadoc)
-	 * @see org.mobicents.servlet.sip.core.timers.ProxyTimerService#cancel(org.mobicents.servlet.sip.proxy.ProxyBranchTimerTask)
-	 */
-	public void cancel(TimerTask task) {
-		task.cancel();
-	}
+    // Counts the number of cancelled tasks
+    private static volatile int numCancelled = 0;
 
-	/* (non-Javadoc)
-	 * @see org.mobicents.servlet.sip.core.timers.ProxyTimerService#isStarted()
-	 */
-	public boolean isStarted() {
-		return started.get();
-	}
+    public ProxyTimerServiceImpl(){
+        super();
+        schedulePurgeTaskIfNeeded();
+    }
 
-	/* (non-Javadoc)
-	 * @see org.mobicents.servlet.sip.core.timers.ProxyTimerService#schedule(org.mobicents.servlet.sip.proxy.ProxyBranchTimerTask, long)
-	 */
-	public void schedule(TimerTask task, long delay) {
-		super.schedule(task, delay);
-	}
+    private void schedulePurgeTaskIfNeeded() {
+        int purgePeriod = StaticServiceHolder.sipStandardService.getCanceledTimerTasksPurgePeriod();
+        if(purgePeriod > 0) {
+            TimerTask t = new TimerTask() {
 
-	/* (non-Javadoc)
-	 * @see org.mobicents.servlet.sip.core.timers.ProxyTimerService#stop()
-	 */
-	public void stop() {		
-		started.set(false);
-		super.cancel();
-		if(logger.isDebugEnabled()) {
-			logger.debug("Stopped proxy timer service "+ this);
-		}
-	}
-	/* (non-Javadoc)
-	 * @see org.mobicents.servlet.sip.core.timers.ProxyTimerService#start()
-	 */
-	public void start() {	
-		started.set(true);
-		if(logger.isDebugEnabled()) {
-			logger.debug("Started proxy timer service "+ this);
-		}
-	}
+                @Override
+                public void run() {
+                    try {
+                        if(logger.isDebugEnabled()) {
+                            logger.debug("Purging canceled timer tasks...");
+                        }
+                        purge();
+                        if(logger.isDebugEnabled()) {
+                            logger.debug("Purging canceled timer tasks completed.");
+                        }
+                    }
+                    catch (Exception e) {
+                        logger.error("failed to execute purge",e);
+                    }
+                }
+            };
+            //purgePeriod originally set in minutes, but needed in millis:
+            purgePeriod = purgePeriod * 60 * 1000;
+
+            super.scheduleAtFixedRate(t, purgePeriod, purgePeriod);
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see org.mobicents.servlet.sip.core.timers.ProxyTimerService#cancel(org.mobicents.servlet.sip.proxy.ProxyBranchTimerTask)
+     */
+    public void cancel(TimerTask task) {
+        task.cancel();
+
+        // Purge is expensive when called frequently, only call it every now and then.
+        // We do not sync the numCancelled variable. We dont care about correctness of
+        // the number, and we will still call purge rought once on every 25 cancels.
+        numCancelled++;
+        if(numCancelled % 100 == 0) {
+            super.purge();
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see org.mobicents.servlet.sip.core.timers.ProxyTimerService#isStarted()
+     */
+    public boolean isStarted() {
+        return started.get();
+    }
+
+    /* (non-Javadoc)
+     * @see org.mobicents.servlet.sip.core.timers.ProxyTimerService#schedule(org.mobicents.servlet.sip.proxy.ProxyBranchTimerTask, long)
+     */
+    public void schedule(TimerTask task, long delay) {
+        super.schedule(task, delay);
+    }
+
+    /* (non-Javadoc)
+     * @see org.mobicents.servlet.sip.core.timers.ProxyTimerService#stop()
+     */
+    public void stop() {
+        started.set(false);
+        super.cancel();
+        if(logger.isDebugEnabled()) {
+            logger.debug("Stopped proxy timer service "+ this);
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see org.mobicents.servlet.sip.core.timers.ProxyTimerService#start()
+     */
+    public void start() {
+        started.set(true);
+        if(logger.isDebugEnabled()) {
+            logger.debug("Started proxy timer service "+ this);
+        }
+    }
 }
