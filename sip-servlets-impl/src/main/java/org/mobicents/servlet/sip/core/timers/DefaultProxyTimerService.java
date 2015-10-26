6ed295357f50f66afa578c04d3ff3d0852d9bc9a
alerant.appngin <alerant.appngin@gmail.com>
2015-10-26 16:23:17 +0100

    Changing threads' name in DefaultProxyTimerService
    Changing threads' name in DefaultProxyTimerService

--- sip-servlets-impl/src/main/java/org/mobicents/servlet/sip/core/timers/DefaultProxyTimerService.java
+++ sip-servlets-impl/src/main/java/org/mobicents/servlet/sip/core/timers/DefaultProxyTimerService.java
@@ -39,178 +39,173 @@ import org.mobicents.servlet.sip.utils.NamingThreadFactory;
  *
 */
 public class DefaultProxyTimerService extends ScheduledThreadPoolExecutor implements ProxyTimerService{
+    private static final Logger logger = Logger.getLogger(DefaultProxyTimerService.class
+            .getName());
 
-        private static final Logger logger = Logger.getLogger(DefaultProxyTimerService.class
-                .getName());
+    // Counts the number of cancelled tasks
+    protected static volatile int numCancelled = 0;
 
-        // Counts the number of cancelled tasks
-        protected static volatile int numCancelled = 0;
+    public static final int SCHEDULER_THREAD_POOL_DEFAULT_SIZE = 4;
 
-        public static final int SCHEDULER_THREAD_POOL_DEFAULT_SIZE = 4;
+    public DefaultProxyTimerService() {
+        super(SCHEDULER_THREAD_POOL_DEFAULT_SIZE, new NamingThreadFactory("sip_default_proxy_timer_service"));
+        schedulePurgeTaskIfNeeded();
+    }
 
-        public DefaultProxyTimerService() {
-            super(SCHEDULER_THREAD_POOL_DEFAULT_SIZE, new NamingThreadFactory("sip_proxy_timer_service"));
-            schedulePurgeTaskIfNeeded();
-        }
+    public DefaultProxyTimerService(int corePoolSize) {
+        super(corePoolSize);
+        schedulePurgeTaskIfNeeded();
+    }
 
-        public DefaultProxyTimerService(int corePoolSize) {
-            super(corePoolSize);
-            schedulePurgeTaskIfNeeded();
-        }
+    /**
+     * @param corePoolSize
+     * @param threadFactory
+     */
+    public DefaultProxyTimerService(int corePoolSize,
+            ThreadFactory threadFactory) {
+        super(corePoolSize, threadFactory);
+        schedulePurgeTaskIfNeeded();
+    }
 
-        /**
-         * @param corePoolSize
-         * @param threadFactory
-         */
-        public DefaultProxyTimerService(int corePoolSize,
-                ThreadFactory threadFactory) {
-            super(corePoolSize, threadFactory);
-            schedulePurgeTaskIfNeeded();
-        }
+    /**
+     * @param corePoolSize
+     * @param handler
+     */
+    public DefaultProxyTimerService(int corePoolSize,
+            RejectedExecutionHandler handler) {
+        super(corePoolSize, handler);
+        schedulePurgeTaskIfNeeded();
+    }
 
-        /**
-         * @param corePoolSize
-         * @param handler
-         */
-        public DefaultProxyTimerService(int corePoolSize,
-                RejectedExecutionHandler handler) {
-            super(corePoolSize, handler);
-            schedulePurgeTaskIfNeeded();
-        }
+    /**
+     * @param corePoolSize
+     * @param threadFactory
+     * @param handler
+     */
+    public DefaultProxyTimerService(int corePoolSize,
+            ThreadFactory threadFactory, RejectedExecutionHandler handler) {
+        super(corePoolSize, threadFactory, handler);
+        schedulePurgeTaskIfNeeded();
+    }
 
-        /**
-         * @param corePoolSize
-         * @param threadFactory
-         * @param handler
-         */
-        public DefaultProxyTimerService(int corePoolSize,
-                ThreadFactory threadFactory, RejectedExecutionHandler handler) {
-            super(corePoolSize, threadFactory, handler);
-            schedulePurgeTaskIfNeeded();
-        }
-
-        private void schedulePurgeTaskIfNeeded() {
-            int purgePeriod = StaticServiceHolder.sipStandardService.getCanceledTimerTasksPurgePeriod();
-            if(purgePeriod > 0) {
-                Runnable r = new Runnable() {
-                    public void run() {
-                        try {
-                            if(logger.isDebugEnabled()) {
-                                logger.debug("Purging canceled timer tasks...");
-                            }
-                            purge();
-                            if(logger.isDebugEnabled()) {
-                                logger.debug("Purging canceled timer tasks completed.");
-                            }
-                        }
-                        catch (Exception e) {
-                            logger.error("failed to execute purge",e);
-                        }
-                    }
-                };
-                scheduleWithFixedDelay(r, purgePeriod, purgePeriod, TimeUnit.MINUTES);
-            }
-        }
-
-        /* (non-Javadoc)
-         * @see org.mobicents.servlet.sip.core.timers.ProxyTimerService#cancel(org.mobicents.servlet.sip.proxy.ProxyBranchTimerTask)
-         */
-        public void cancel(TimerTask task) {
-            //CANCEL needs to remove the shceduled timer see http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6602600
-            //to improve perf
-
-            ScheduledFuture<?> future = null;
-            Object[] array = super.getQueue().toArray();
-            for(int i = 0; i<array.length; i++) {
-                if(array[i] instanceof ScheduledFuture<?>){
-                    future = (ScheduledFuture<?>)array[i];
+    private void schedulePurgeTaskIfNeeded() {
+        int purgePeriod = StaticServiceHolder.sipStandardService.getCanceledTimerTasksPurgePeriod();
+        if(purgePeriod > 0) {
+            Runnable r = new Runnable() {
+                public void run() {
                     try {
-
-                        Field callableField = future.getClass().getDeclaredField("callable");
-                        callableField.setAccessible(true);
-                        Object callable = callableField.get(future);
-                        callableField.setAccessible(false);
-
-                        if(callable!=null){
-                            Field taskField = callable.getClass().getDeclaredField("task");
-                            taskField.setAccessible(true);
-                            TimerTask scheduledTask = null;
-                            if(taskField.get(callable) instanceof TimerTask){
-                                 scheduledTask = (TimerTask) taskField.get(callable);
-                            }
-                            taskField.setAccessible(false);
-
-                            if(scheduledTask!=null && scheduledTask.equals(task)){
-                                break;
-                            }
+                        if(logger.isDebugEnabled()) {
+                            logger.debug("Purging canceled timer tasks...");
                         }
-                    } catch (NoSuchFieldException | SecurityException | IllegalAccessException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
+                        purge();
+                        if(logger.isDebugEnabled()) {
+                            logger.debug("Purging canceled timer tasks completed.");
+                        }
+                    }
+                    catch (Exception e) {
+                        logger.error("failed to execute purge",e);
                     }
                 }
-            }
-
-            if(future != null) {
-                boolean removed = super.remove((Runnable) future);
-                if(logger.isDebugEnabled()) {
-                    logger.debug("expiration timer on sip proxy task" + task + " removed : " + removed);
-                }
-                boolean cancelled = future.cancel(true);
-                if(logger.isDebugEnabled()) {
-                    logger.debug("expiration timer on sip proxy task" + task + " Cancelled : " + cancelled);
-                }
-
-                future = null;
-                // Purge is expensive when called frequently, only call it every now and then.
-                // We do not sync the numCancelled variable. We dont care about correctness of
-                // the number, and we will still call purge rought once on every 25 cancels.
-                numCancelled++;
-                if(numCancelled % 100 == 0) {
-                    super.purge();
-                }
-            } else {
-                if(logger.isDebugEnabled()) {
-                    logger.debug("expiration timer future is null, thus cannot be Cancelled");
-                }
-            }
+            };
+            scheduleWithFixedDelay(r, purgePeriod, purgePeriod, TimeUnit.MINUTES);
         }
+    }
 
-        /* (non-Javadoc)
-         * @see org.mobicents.servlet.sip.core.timers.ProxyTimerService#isStarted()
-         */
-        public boolean isStarted() {
-            return super.isTerminated();
-        }
+    /* (non-Javadoc)
+     * @see org.mobicents.servlet.sip.core.timers.ProxyTimerService#cancel(org.mobicents.servlet.sip.proxy.ProxyBranchTimerTask)
+     */
+    public void cancel(TimerTask task) {
+        //CANCEL needs to remove the shceduled timer see http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6602600
+        //to improve perf
 
-        /* (non-Javadoc)
-         * @see org.mobicents.servlet.sip.core.timers.ProxyTimerService#schedule(org.mobicents.servlet.sip.proxy.ProxyBranchTimerTask, long)
-         */
-        public void schedule(TimerTask task, long delay) {
-            super.schedule(task, delay, TimeUnit.MILLISECONDS);
-        }
+        ScheduledFuture<?> future = null;
+        Object[] array = super.getQueue().toArray();
+        for(int i = 0; i<array.length; i++) {
+            if(array[i] instanceof ScheduledFuture<?>){
+                future = (ScheduledFuture<?>)array[i];
+                try {
 
-        /* (non-Javadoc)
-         * @see org.mobicents.servlet.sip.core.timers.ProxyTimerService#stop()
-         */
-        public void stop() {
-            super.shutdownNow();
-            if(logger.isInfoEnabled()) {
-                logger.info("Stopped timer service "+ this);
+                    Field callableField = future.getClass().getDeclaredField("callable");
+                    callableField.setAccessible(true);
+                    Object callable = callableField.get(future);
+                    callableField.setAccessible(false);
+
+                    if(callable!=null){
+                        Field taskField = callable.getClass().getDeclaredField("task");
+                        taskField.setAccessible(true);
+                        TimerTask scheduledTask = null;
+                        if(taskField.get(callable) instanceof TimerTask){
+                             scheduledTask = (TimerTask) taskField.get(callable);
+                        }
+                        taskField.setAccessible(false);
+
+                        if(scheduledTask!=null && scheduledTask.equals(task)){
+                            break;
+                        }
+                    }
+                } catch (NoSuchFieldException | SecurityException | IllegalAccessException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
             }
         }
 
-        /* (non-Javadoc)
-         * @see org.mobicents.servlet.sip.core.timers.ProxyTimerService#start()
-         */
-        public void start() {
-            prestartAllCoreThreads();
-            if(logger.isInfoEnabled()) {
-                logger.info("Started timer service "+ this);
+        if(future != null) {
+            boolean removed = super.remove((Runnable) future);
+            if(logger.isDebugEnabled()) {
+                logger.debug("expiration timer on sip proxy task" + task + " removed : " + removed);
+            }
+            boolean cancelled = future.cancel(true);
+            if(logger.isDebugEnabled()) {
+                logger.debug("expiration timer on sip proxy task" + task + " Cancelled : " + cancelled);
+            }
+
+            future = null;
+            // Purge is expensive when called frequently, only call it every now and then.
+            // We do not sync the numCancelled variable. We dont care about correctness of
+            // the number, and we will still call purge rought once on every 25 cancels.
+            numCancelled++;
+            if(numCancelled % 100 == 0) {
+                super.purge();
+            }
+        } else {
+            if(logger.isDebugEnabled()) {
+                logger.debug("expiration timer future is null, thus cannot be Cancelled");
             }
         }
     }
 
-    
-    
+    /* (non-Javadoc)
+     * @see org.mobicents.servlet.sip.core.timers.ProxyTimerService#isStarted()
+     */
+    public boolean isStarted() {
+        return super.isTerminated();
+    }
+
+    /* (non-Javadoc)
+     * @see org.mobicents.servlet.sip.core.timers.ProxyTimerService#schedule(org.mobicents.servlet.sip.proxy.ProxyBranchTimerTask, long)
+     */
+    public void schedule(TimerTask task, long delay) {
+        super.schedule(task, delay, TimeUnit.MILLISECONDS);
+    }
+
+    /* (non-Javadoc)
+     * @see org.mobicents.servlet.sip.core.timers.ProxyTimerService#stop()
+     */
+    public void stop() {
+        super.shutdownNow();
+        if(logger.isInfoEnabled()) {
+            logger.info("Stopped timer service "+ this);
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see org.mobicents.servlet.sip.core.timers.ProxyTimerService#start()
+     */
+    public void start() {
+        prestartAllCoreThreads();
+        if(logger.isInfoEnabled()) {
+            logger.info("Started timer service "+ this);
+        }
+    }
 }
