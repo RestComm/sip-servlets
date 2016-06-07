dbc00d68ab94348d6111237d62dbc757131dec82
Gabor Balogh <balogh.gabor@alerant.hu>
2016-06-07 12:52:47 +0200

    Add bug fixes for issue#133
    Fix url encoding
    Fix servlet mapping handling
    Add missing module dependecy
    Fix isConvergedDeployment method in ConvergedServletContainerImpl

--- containers/sip-servlets-as8/src/main/java/org/mobicents/io/undertow/servlet/core/LifecyleInterceptorInvocation.java
+++ containers/sip-servlets-as8/src/main/java/org/mobicents/io/undertow/servlet/core/LifecyleInterceptorInvocation.java
@@ -18,19 +18,26 @@
  */
 package org.mobicents.io.undertow.servlet.core;
 
-import io.undertow.servlet.api.FilterInfo;
-import io.undertow.servlet.api.LifecycleInterceptor;
-import io.undertow.servlet.api.ServletInfo;
+import java.util.List;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterConfig;
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import java.util.List;
+
+import org.mobicents.servlet.sip.startup.ConvergedServletContextImpl;
+
+import io.undertow.servlet.api.FilterInfo;
+import io.undertow.servlet.api.LifecycleInterceptor;
+import io.undertow.servlet.api.ServletInfo;
+import io.undertow.servlet.handlers.DefaultServlet;
 
 /**
  * Utility class for invoking servlet and filter lifecycle methods.
+ * 
+ * @author kakonyi.istvan@alerant.hu
+ * @author balogh.gabor@alerant.hu
  */
 public class LifecyleInterceptorInvocation implements LifecycleInterceptor.LifecycleContext {
     private final List<LifecycleInterceptor> list;
@@ -40,13 +47,16 @@ public class LifecyleInterceptorInvocation implements LifecycleInterceptor.Lifec
     private final Filter filter;
     private int i;
     private final ServletConfig servletConfig;
+    private final ServletConfig convergedServletConfig;
     private final FilterConfig filterConfig;
 
-    public LifecyleInterceptorInvocation(List<LifecycleInterceptor> list, ServletInfo servletInfo, Servlet servlet,  ServletConfig servletConfig) {
+    public LifecyleInterceptorInvocation(List<LifecycleInterceptor> list, ServletInfo servletInfo, Servlet servlet,
+            ServletConfig servletConfig, ServletConfig convergedServletConfig) {
         this.list = list;
         this.servletInfo = servletInfo;
         this.servlet = servlet;
         this.servletConfig = servletConfig;
+        this.convergedServletConfig = convergedServletConfig;
         this.filter = null;
         this.filterConfig = null;
         this.filterInfo = null;
@@ -59,15 +69,18 @@ public class LifecyleInterceptorInvocation implements LifecycleInterceptor.Lifec
         this.servletInfo = servletInfo;
         this.filterInfo = null;
         this.servletConfig = null;
+        this.convergedServletConfig = null;
         this.filter = null;
         this.filterConfig = null;
         i = list.size();
     }
 
-    public LifecyleInterceptorInvocation(List<LifecycleInterceptor> list, FilterInfo filterInfo, Filter filter,  FilterConfig filterConfig) {
+    public LifecyleInterceptorInvocation(List<LifecycleInterceptor> list, FilterInfo filterInfo, Filter filter,
+            FilterConfig filterConfig) {
         this.list = list;
         this.servlet = null;
         this.servletConfig = null;
+        this.convergedServletConfig = null;
         this.filter = filter;
         this.filterConfig = filterConfig;
         this.filterInfo = filterInfo;
@@ -90,31 +103,35 @@ public class LifecyleInterceptorInvocation implements LifecycleInterceptor.Lifec
     public void proceed() throws ServletException {
         if (--i >= 0) {
             final LifecycleInterceptor next = list.get(i);
-            if(filter != null) {
-                if(filterConfig == null) {
+            if (filter != null) {
+                if (filterConfig == null) {
                     next.destroy(filterInfo, filter, this);
                 } else {
                     next.init(filterInfo, filter, this);
                 }
             } else {
-                if(servletConfig == null) {
+                if (servletConfig == null && convergedServletConfig == null) {
                     next.destroy(servletInfo, servlet, this);
                 } else {
                     next.init(servletInfo, servlet, this);
                 }
             }
         } else if (i == -1) {
-            if(filter != null) {
-                if(filterConfig == null) {
+            if (filter != null) {
+                if (filterConfig == null) {
                     filter.destroy();
                 } else {
                     filter.init(filterConfig);
                 }
             } else {
-                if(servletConfig == null) {
+                if (servletConfig == null && convergedServletConfig == null) {
                     servlet.destroy();
                 } else {
-                    servlet.init(servletConfig);
+                    if (servlet instanceof DefaultServlet) {
+                        servlet.init(servletConfig);
+                    } else {
+                        servlet.init(convergedServletConfig);
+                    }
                 }
             }
         }
