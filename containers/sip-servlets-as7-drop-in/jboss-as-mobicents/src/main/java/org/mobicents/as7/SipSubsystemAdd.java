bef2b70405cbdcd2135edf5f8c9250dcd65bd519
Gergely Pósfai <posfaig@gmail.com>
2016-08-01 21:09:53 +0200

    Moving on to new JAIN SIP HA version, and removing a no longer necessary SipSubsystem dependency.

--- containers/sip-servlets-as7-drop-in/jboss-as-mobicents/src/main/java/org/mobicents/as7/SipSubsystemAdd.java
+++ containers/sip-servlets-as7-drop-in/jboss-as-mobicents/src/main/java/org/mobicents/as7/SipSubsystemAdd.java
@@ -256,7 +256,6 @@ class SipSubsystemAdd extends AbstractBoottimeAddStepHandler {
         newControllers.add(context.getServiceTarget().addService(SipSubsystemServices.JBOSS_SIP, service)
                 .addDependency(PathManagerService.SERVICE_NAME, PathManager.class, service.getPathManagerInjector())
                 .addDependency(DependencyType.OPTIONAL, ServiceName.JBOSS.append("mbean", "server"), MBeanServer.class, service.getMbeanServer())
-                .addDependencies(ServiceName.JBOSS.append("cluster", "siprep"))
                 .setInitialMode(Mode.ACTIVE)
                 .install());
 
