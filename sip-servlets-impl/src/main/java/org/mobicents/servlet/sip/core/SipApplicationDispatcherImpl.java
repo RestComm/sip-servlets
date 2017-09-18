e23c85b3bd282255a61ab049b06ef29e0b1114f5
Jaime Casero <jaime.casero@telestax.com>
2017-09-18 13:59:57 -0400

    fixed project type to community after merge with telscale branch

--- sip-servlets-impl/src/main/java/org/mobicents/servlet/sip/core/SipApplicationDispatcherImpl.java
+++ sip-servlets-impl/src/main/java/org/mobicents/servlet/sip/core/SipApplicationDispatcherImpl.java
@@ -457,7 +457,7 @@ public class SipApplicationDispatcherImpl implements SipApplicationDispatcher, S
                             //define remote server address (optionally)
                     statsReporter.setRemoteServer(statisticsServer);
                     String projectName = System.getProperty("RestcommProjectName", "sipservlets");
-                    String projectType = System.getProperty("RestcommProjectType", "product");
+                    String projectType = System.getProperty("RestcommProjectType", "community");
                     String projectVersion = System.getProperty("RestcommProjectVersion", Version.getVersionProperty(Version.RELEASE_VERSION));
                     if(logger.isDebugEnabled()) {
                                     logger.debug("Restcomm Stats " + projectName + " " + projectType + " " + projectVersion);
